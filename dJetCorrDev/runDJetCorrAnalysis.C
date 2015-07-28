c2a12d12abc073dcbd741c6bf05674d3d58ab17a
saiola <salvatore.aiola@cern.ch>
2015-07-28 18:47:01 +0200

    Small changes

--- dJetCorrDev/runDJetCorrAnalysis.C
+++ dJetCorrDev/runDJetCorrAnalysis.C
@@ -2,21 +2,30 @@
 
 class DJetCorrAnalysis;
 
-void runDJetCorrAnalysis(const char* options = "run plot", const char* train = "Jets_EMC_pp_407_408_409_410", const char* inputPath = "$JETRESULTS")
+DJetCorrAnalysis* runDJetCorrAnalysis(const char* options = "plot", const char* train = "Jets_EMC_pp_MC_502_503_504_505",
+                                      Bool_t isMC = kTRUE, Bool_t loadLibs = kTRUE, const char* inputPath = "$JETRESULTS")
 {
   TGaxis::SetMaxDigits(3); 
   
   TString tracksName = "tracks";
   //TString tracksD0Name = tracksName;
   //TString tracksDStarName = tracksName;
-  TString tracksD0Name = "DcandidatesAndTracksD0rec";
-  TString tracksDStarName = "DcandidatesAndTracksDStarrec";
+  TString tracksD0Name = "DcandidatesAndTracksD0";
+  TString tracksDStarName = "DcandidatesAndTracksDStar";
 
-  gROOT->LoadMacro("MassFitter.cxx+g");
-  gROOT->LoadMacro("DJetCorrAnalysisParams.cxx+g");
-  gROOT->LoadMacro("DJetCorrBase.cxx+g");
-  gROOT->LoadMacro("DJetCorrAnalysis.cxx+g");
+  if (!isMC) {
+    tracksD0Name += "rec";
+    tracksDStarName += "rec";
+  }
 
+  if (loadLibs) {
+    gROOT->LoadMacro("HistoStyler.cxx+g");
+    gROOT->LoadMacro("MassFitter.cxx+g");
+    gROOT->LoadMacro("DJetCorrAnalysisParams.cxx+g");
+    gROOT->LoadMacro("DJetCorrBase.cxx+g");
+    gROOT->LoadMacro("DJetCorrAnalysis.cxx+g");
+  }
+  
   DJetCorrAnalysis* projDjet = new DJetCorrAnalysis(train);
   
   projDjet->SetOverwrite(kTRUE);
@@ -30,11 +39,11 @@ void runDJetCorrAnalysis(const char* options = "run plot", const char* train = "
   projDjet->SetPlotFormat("pdf");
   projDjet->SetSavePlots(kTRUE);
 
-  //projDjet->AddAnalysisParams("D0", "Full", "R040", tracksD0Name);
-  projDjet->AddAnalysisParams("D0", "Full", "R060", tracksD0Name);
+  //projDjet->AddAnalysisParams("D0", "Full", "R040", tracksD0Name, isMC);
+  projDjet->AddAnalysisParams("D0", "Full", "R060", tracksD0Name, isMC);
 
-  //projDjet->AddAnalysisParams("DStar", "Charged", "R040", tracksDStarName);
-  projDjet->AddAnalysisParams("DStar", "Charged", "R060", tracksDStarName);
+  //projDjet->AddAnalysisParams("DStar", "Charged", "R040", tracksDStarName, isMC);
+  projDjet->AddAnalysisParams("DStar", "Charged", "R060", tracksDStarName, isMC);
 
   TString opt(options);
   TObjArray *optList = opt.Tokenize(" ");
@@ -43,13 +52,17 @@ void runDJetCorrAnalysis(const char* options = "run plot", const char* train = "
     projDjet->GenerateQAHistograms();
     projDjet->GenerateDJetCorrHistograms();
     
-    projDjet->SaveOutputFile();
+    if (!optList->Contains("plot")) projDjet->SaveOutputFile();
   }
 
   if (optList->Contains("plot")) {
     projDjet->PlotTrackHistograms();
     projDjet->PlotDJetCorrHistograms();
+
+    projDjet->SaveOutputFile();
   }
+
+  return projDjet;
 }
 
 
