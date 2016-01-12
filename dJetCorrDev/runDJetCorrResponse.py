45b9ad41ad57ac0e397b5674da47ee303a1a9db8
saiola <salvatore.aiola@cern.ch>
2016-01-12 16:03:03 -0500

    Python scripts

---
+++ dJetCorrDev/runDJetCorrResponse.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env ipython -i
+#python script to use the class DJetCorrResponse
+
+import ROOT
+import argparse
+from ROOT import gROOT
+from ROOT import TGaxis
+
+import IPython
+
+import commonFunctions
+
+def runDJetCorrResponse(train, run=True, plot=True, loadLibs=True, inputPath="$JETRESULTS"):
+    
+    TGaxis.SetMaxDigits(3)
+  
+    tracksD0Name = "DcandidatesAndTracksD0"
+    tracksDStarName = "DcandidatesAndTracksDStar"
+    
+    if loadLibs:
+        commonFunctions.LoadDJetCorrClasses()
+
+    from ROOT import DJetCorrResponse
+  
+    projDjet = DJetCorrResponse(train);
+
+    #projDjet->SetEfficiencyMode("TH1")
+    projDjet.SetOverwrite(True)
+    projDjet.SetInputPath(inputPath)
+        
+    projDjet.SetPlotFormat("pdf")
+    projDjet.SetSavePlots(False)
+        
+    #projDjet.AddAnalysisParams("D0", "Full", "R040", tracksD0Name)
+    projDjet.AddAnalysisParams("D0", "Full", "R060", tracksD0Name)
+        
+    #projDjet.AddAnalysisParams("DStar", "Charged", "R040", tracksDStarName)
+    projDjet.AddAnalysisParams("DStar", "Charged", "R060", tracksDStarName)
+          
+    if run:
+        projDjet.ProjectResponseMatrices()
+        projDjet.SaveOutputFile()
+        
+    if plot:
+        projDjet.PlotResponseMatrices()
+
+    projDjet.UpdateAllCanvases()
+    return projDjet;
+
+if __name__ == '__main__':
+    # runDJetCorrResponse.py executed as script
+    
+    parser = argparse.ArgumentParser(description='D meson jet correlation response matrix.')
+    parser.add_argument('train', metavar='train',
+                        help='Train to be analyzed')
+    parser.add_argument('-r', '--run', action='store_const',
+                        default=False, const=True,
+                        help='Run the analysis')
+    parser.add_argument('-p', '--plot', action='store_const',
+                        default=False, const=True,
+                        help='Plot the results')
+    parser.add_argument('--no-Libs', action='store_const',
+                        default=False, const=True,
+                        help='Load the DJetCorr libraries')
+    parser.add_argument('--inputPath', metavar='inputPath',
+                        default="$JETRESULTS",
+                        help='Input path')
+    args = parser.parse_args()
+    
+    runDJetCorrResponse(args.train, args.run, args.plot, not args.no_Libs, args.inputPath)
