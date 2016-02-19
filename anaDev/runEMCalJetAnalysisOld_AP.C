e88a19ecdc597cc9c9f1e7e0105974441cf6f6a1
saiola <salvatore.aiola@cern.ch>
2016-02-18 19:40:13 -0500

    Adapt to modifications in jet framework

--- anaDev/runEMCalJetAnalysisOld_AP.C
+++ anaDev/runEMCalJetAnalysisOld_AP.C
@@ -1,12 +1,17 @@
 void runEMCalJetAnalysisOld_AP(
 			       const char   *cDataType      = "AOD",                                   // set the analysis type, AOD or ESD
-			       const char   *cLocalFiles    = "fileLists/files_LHC11h_2_AOD145.txt",   // set the local list file
+			       const char   *cLocalFiles    = "fileLists/files_LHC12c_pass2_AOD.txt",   // set the local list file
 			       UInt_t        iNumFiles      = 100,                                     // number of files analyzed locally
-			       UInt_t        iNumEvents     = 100,                                    // number of events to be analyzed
-			       const char   *cRunPeriod     = "LHC11h",                                // set the run period
+			       UInt_t        iNumEvents     = 20000,                                    // number of events to be analyzed
+			       const char   *cRunPeriod     = "LHC12c",                                // set the run period
 			       const char   *cTaskName      = "JetAna"
 			       )
 {
+  // AliEmcalPhysicsSelection::kEmcalOk, AliEmcalPhysicsSelection::kEmcalH,
+  // AliVEvent::kINT7, AliVEvent::kMB, AliVEvent::kCentral, AliVEvent::kSemiCentral,
+  // AliVEvent::kEMCEGA, AliVEvent::kEMCEJE
+  UInt_t kPhysSel = AliVEvent::kAnyINT;
+  
   enum eDataType { kAod, kEsd };
 
   eDataType iDataType;
@@ -21,26 +26,31 @@ void runEMCalJetAnalysisOld_AP(
     Printf("datatype = AOD or ESD");
     return;
   }
-  
+
+  Bool_t doNotStart = kTRUE;
+  TString OCDBpath = "local:///Volumes/DATA/ALICE/OCDB/2012";
+    
   gROOT->LoadMacro("$ALICE_PHYSICS/PWGJE/EMCALJetTasks/macros/runEMCalJetAnalysisOld.C");
-  AliAnalysisManager* pMgr = runEMCalJetAnalysisOld(cDataType, cLocalFiles, iNumFiles, iNumEvents, cRunPeriod, cTaskName, kTRUE, kTRUE, "local:///Volumes/DATA/ALICE/OCDB/2011", kTRUE);
+  AliAnalysisManager* pMgr = runEMCalJetAnalysisOld(cDataType, cLocalFiles, iNumFiles, iNumEvents, cRunPeriod, kPhysSel, cTaskName, kTRUE, kTRUE, OCDBpath, doNotStart);
 
-  //pMgr->SetUseProgressBar(kFALSE, 250);
-  //pMgr->SetDebugLevel(1);
+  if (doNotStart) {
+    //pMgr->SetUseProgressBar(kFALSE, 10);
+    //pMgr->SetDebugLevel(2);
   
-  //pMgr->AddClassDebug("AliEmcalJetTask", AliLog::kDebug+100);
+    //pMgr->AddClassDebug("AliEmcalJetTask", AliLog::kDebug+100);
 
-  TChain* pChain = 0;
-  if (iDataType == kAod) {
-    gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/CreateAODChain.C");
-    pChain = CreateAODChain(cLocalFiles, iNumFiles, 0, kFALSE);
-  }
-  else {
-    gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/CreateESDChain.C");
-    pChain = CreateESDChain(cLocalFiles, iNumFiles, 0, kFALSE);
-  }
+    TChain* pChain = 0;
+    if (iDataType == kAod) {
+      gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/CreateAODChain.C");
+      pChain = CreateAODChain(cLocalFiles, iNumFiles, 0, kFALSE);
+    }
+    else {
+      gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/CreateESDChain.C");
+      pChain = CreateESDChain(cLocalFiles, iNumFiles, 0, kFALSE);
+    }
 
-  // start analysis
-  Printf("Starting Analysis...");
-  pMgr->StartAnalysis("local", pChain, iNumEvents);
+    // start analysis
+    Printf("Starting Analysis...");
+    pMgr->StartAnalysis("local", pChain, iNumEvents);
+  }
 }
