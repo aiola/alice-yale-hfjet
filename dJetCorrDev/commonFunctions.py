e7b92d4e8658d223a7333c3704d544ff7c26d1dd
saiola <salvatore.aiola@cern.ch>
2016-03-10 18:36:35 -0500

    cleanup

--- dJetCorrDev/commonFunctions.py
+++ dJetCorrDev/commonFunctions.py
@@ -2,10 +2,4 @@ import ROOT
 
 def LoadDJetCorrClasses():
     ROOT.gSystem.Load("DJetCorr.so")
-    #gROOT.LoadMacro("HistoStyler.cxx+g")
-    #gROOT.LoadMacro("MassFitter.cxx+g")
-    #gROOT.LoadMacro("DJetCorrAnalysisParams.cxx+g")
-    #gROOT.LoadMacro("DJetCorrBase.cxx+g")
-    #gROOT.LoadMacro("DJetCorrAnalysis.cxx+g")
-    #gROOT.LoadMacro("DJetCorrResponse.cxx+g")
-    #gROOT.LoadMacro("DJetCorrUnfold.cxx+g")
+    
