89e88ff571908066871ae17382a8b472947d6a3f
saiola <salvatore.aiola@cern.ch>
2016-05-26 13:44:23 -0400

    Add scripts to run automatic tests

---
+++ anaDev/runEMCalTest.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+chmod +x $ALICE_PHYSICS/PWG/EMCAL/macros/runEMCalSampleTask.sh
+$ALICE_PHYSICS/PWG/EMCAL/macros/runEMCalSampleTask.sh AOD LHC12c fileLists/files_LHC12c_pass2_AOD.txt 5000
