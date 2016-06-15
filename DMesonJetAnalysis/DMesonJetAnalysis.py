5fa6b59cc222a37136e0090cf3da64514ba9325a
saiola <salvatore.aiola@cern.ch>
2016-06-14 23:22:33 -0400

    updates

--- DMesonJetAnalysis/DMesonJetAnalysis.py
+++ DMesonJetAnalysis/DMesonJetAnalysis.py
@@ -24,10 +24,22 @@ class DMesonJetAnalysisEngine:
         self.PlotInvMassPlots()
         
     def PlotInvMassPlots(self):
-        for bin in self.fBinSet.fBins:
+        for name,bins in self.fBinSet.fBins.iteritems():
+            self.PlotInvMassPlotsBinSet(name,bins)
+    
+    def GenerateInvMassCanvas(self, name, n):
+        rows = int(math.floor(math.sqrt(n)))
+        cols = int(math.ceil(float(n) / rows))
+        c = ROOT.TCanvas(name, name, cols*400, rows*400)
+        c.Divide(cols, rows)
+        globalList.append(c)
+        return c
+    
+    def PlotInvMassPlotsBinSet(self, name, bins):
+        c = self.GenerateInvMassCanvas(name, len(bins))
+        for i,bin in enumerate(bins):
             if bin.fInvMassHisto:
-                c = ROOT.TCanvas()
-                globalList.append(c)
+                c.cd(i+1)
                 bin.fInvMassHisto.Draw()
 
 class DMesonJetAnalysis:
@@ -41,7 +53,7 @@ class DMesonJetAnalysis:
     def StartAnalysis(self, config):
         binSet = DMesonJetProjectors.BinSet()
         for binLists in config["bins"]:
-            binSet.AddBins(jetPtLimits = binLists["jet_pt"], DPtLimits = binLists["d_pt"], ZLimits = binLists["d_z"])
+            binSet.AddBins(name = binLists["name"], jetPtLimits = binLists["jet_pt"], DPtLimits = binLists["d_pt"], ZLimits = binLists["d_z"])
          
         eng = DMesonJetAnalysisEngine(config["trigger"], config["d_meson"], 
                                      binSet, config["n_mass_bins"], config["min_mass"], config["max_mass"],
--- DMesonJetAnalysis/DMesonJetProjectors.py
+++ DMesonJetAnalysis/DMesonJetProjectors.py
@@ -15,18 +15,20 @@ def find_file(path, file_name):
 
 class BinSet:
     def __init__(self):
-        self.fBins = []
+        self.fBins = dict()
         
-    def AddBins(self, jetPtLimits = [0, -1], ZLimits = [0, -1], DPtLimits = [0, -1]):
+    def AddBins(self, name, jetPtLimits = [0, -1], ZLimits = [0, -1], DPtLimits = [0, -1]):
+        self.fBins[name] = []
         for _DPtMin,_DPtMax in zip(DPtLimits[:-1],DPtLimits[1:]):
             for _jetPtMin,_jetPtMax in zip(jetPtLimits[:-1],jetPtLimits[1:]):
                 for _DZMin,_DZMax in zip(ZLimits[:-1],ZLimits[1:]):
-                    self.fBins.append(BinLimits(jetPtMin=_jetPtMin, jetPtMax=_jetPtMax, DPtMin = _DPtMin, DPtMax = _DPtMax, DZMin = _DZMin, DZMax = _DZMax))
+                    self.fBins[name].append(BinLimits(jetPtMin=_jetPtMin, jetPtMax=_jetPtMax, DPtMin = _DPtMin, DPtMax = _DPtMax, DZMin = _DZMin, DZMax = _DZMax))
 
     def FindBin(self, dmeson, jetDef):
-        for bin in self.fBins:
-            if bin.IsInBinLimits(dmeson, jetDef):
-                yield bin
+        for bins in self.fBins.itervalues():
+            for bin in bins:
+                if bin.IsInBinLimits(dmeson, jetDef):
+                    yield bin
 
 class BinLimits:
     def __init__(self, jetPtMin = 0, jetPtMax = -1, DPtMin = 0, DPtMax = -1, DZMin = 0, DZMax = -1):
@@ -86,13 +88,13 @@ class BinLimits:
     def GetTitle(self):
         title = ""
         if self.fDPtMax > self.fDPtMin:
-            title += "{0:.2f} < #it{{p}}_{{T,D}} < {0:.2f} GeV/#it{{c}}, ".format(self.fDPtMin, self.fDPtMax)
+            title += "{0:.2f} < #it{{p}}_{{T,D}} < {1:.2f} GeV/#it{{c}}, ".format(self.fDPtMin, self.fDPtMax)
         
         if self.fJetPtMax > self.fJetPtMin:
-            title += "{0:.2f} < #it{{p}}_{{T,jet}} < {0:.2f} GeV/#it{{c}}, ".format(self.fJetPtMin, self.fJetPtMax)
+            title += "{0:.2f} < #it{{p}}_{{T,jet}} < {1:.2f} GeV/#it{{c}}, ".format(self.fJetPtMin, self.fJetPtMax)
             
         if self.fDZMax > self.fDZMin:
-            title += "{0:.2f} < #it{{z}}_{{||, D}} < {0:.2f}, ".format(self.fDZMin, self.fDZMax)
+            title += "{0:.2f} < #it{{z}}_{{||, D}} < {1:.2f}, ".format(self.fDZMin, self.fDZMax)
         
         #remove last ", "
         if title:
@@ -103,14 +105,18 @@ class BinLimits:
         hname = "InvMass_{0}_{1}_{2}".format(trigger, DMesonDef, self.GetName())
         htitle = "{0} - {1} Invariant Mass: {2}".format(trigger, DMesonDef, self.GetTitle())
         self.fInvMassHisto = ROOT.TH1D(hname, htitle, nMassBins, minMass, maxMass)
+        self.fInvMassHisto.Sumw2()
+        self.fInvMassHisto.SetMarkerSize(0.9)
+        self.fInvMassHisto.SetMarkerStyle(ROOT.kFullCircle)
         globalList.append(self.fInvMassHisto)
         
 class DMesonJetDataProjector:
-    def __init__(self, inputPath, train, fileName, taskName):
+    def __init__(self, inputPath, train, fileName, taskName, maxEvents):
         self.fInputPath = inputPath
         self.fTrain = train
         self.fFileName = fileName
         self.fTaskName = taskName
+        self.fMaxEvents = maxEvents
         self.fChains = dict()
         
     def GenerateChain(self, treeName):
@@ -136,10 +142,15 @@ class DMesonJetDataProjector:
         chain = self.GenerateChain(treeName)
         
         print("Running analysis on tree {0}. Total number of entries is {1}".format(treeName, chain.GetEntries()))
+        if self.fMaxEvents > 0:
+            print("The analysis will stop at the {0} entry.".format(self.fMaxEvents))
         
         for i,dmeson in enumerate(chain):
             if i % 10000 == 0:
                 print("D meson candidate n. {0}".format(i))
+                if self.fMaxEvents > 0 and i > self.fMaxEvents:
+                    print("Stopping the analysis.")
+                    break
             for jetDef in jetDefinitions:
                 bins = binSet.FindBin(dmeson, jetDef)
                 
