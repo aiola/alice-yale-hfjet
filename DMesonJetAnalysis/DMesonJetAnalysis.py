d9a16b4176bb942be932a7b6f47c8cb7d1706674
saiola <salvatore.aiola@cern.ch>
2016-06-16 17:05:10 -0400

    Add spectra generation

--- DMesonJetAnalysis/DMesonJetAnalysis.py
+++ DMesonJetAnalysis/DMesonJetAnalysis.py
@@ -4,11 +4,31 @@
 import ROOT
 import math
 import DMesonJetProjectors
+from array import array
 
 globalList = []
 
+class Axis:
+    def __init__(self, name, title, bins):
+        self.fName = name
+        self.fTitle = title
+        self.fBins = bins
+
+class Spectrum:
+    def __init__(self, config):
+        self.fName = config["name"]
+        self.fBins = config["bins"]
+        self.fAxis = []
+        if config["jet_pt"]:
+            self.fAxis.append(Axis("jet_pt", "#it{p}_{T,jet} GeV/#it{c}", config["jet_pt"]))
+        if config["d_pt"]:
+            self.fAxis.append(Axis("d_pt", "#it{p}_{T,D} GeV/#it{c}", config["d_pt"]))
+        if config["d_z"]:
+            self.fAxis.append(Axis("d_z", "#it{z}_{||,D}", config["d_z"]))
+        self.fHistogram = None
+
 class DMesonJetAnalysisEngine:
-    def __init__(self, trigger, dmeson, binSet, nMassBins, minMass, maxMass, jets, projector):
+    def __init__(self, trigger, dmeson, binSet, nMassBins, minMass, maxMass, jets, spectra, projector):
         self.fTrigger = trigger
         self.fDMeson = dmeson
         self.fJetDefinitions = jets
@@ -17,6 +37,9 @@ class DMesonJetAnalysisEngine:
         self.fNMassBins = nMassBins
         self.fMinMass = minMass
         self.fMaxMass = maxMass
+        self.fSpectra = dict()
+        for s in spectra:
+            self.fSpectra[s["name"]] = Spectrum(s)
         
     def CreateMassFitter(self, name):
         if self.fDMeson == "D0":
@@ -45,15 +68,99 @@ class DMesonJetAnalysisEngine:
         
         self.FitInvMassPlots()
         self.PlotInvMassPlots()
+        self.GenerateSpectra()
+        self.PlotSpectra()
         
+    def PlotSpectra(self):
+        for s in self.fSpectra.itervalues():
+            if len(s.fAxis) == 1:
+                self.PlotSpectrum1D(s)
+            elif len(s.fAxis) == 2:
+                self.PlotSpectrum2D(s)
+            elif len(s.fAxis) == 3:
+                self.PlotSpectrum3D(s)
+            else:
+                print("Not able to plot spectra with dim > 3!")
+    
+    def PlotSpectrum1D(self, s):
+        c = ROOT.TCanvas("{0}_canvas".format(s.fName), s.fName)
+        c.SetLogy()
+        c.cd()
+        h = s.fHistogram.DrawCopy()
         
+        h.SetMarkerColor(ROOT.kBlue+2)
+        h.SetMarkerStyle(ROOT.kFullCircle)
+        h.SetMarkerSize(0.9)
+        h.SetLineColor(ROOT.kBlue+2)
+        
+        globalList.append(c)
+        globalList.append(h)
+        
+    def PlotSpectrum2D(self, s):
+        c = ROOT.TCanvas("{0}_canvas".format(s.fName), s.fName)
+        c.SetLogz()
+        c.cd()
+        h = s.fHistogram.DrawCopy("colz")
+        
+        globalList.append(c)
+        globalList.append(h)       
+        
+    def PlotSpectrum3D(self, s):
+        print("PlotSpectrum3D not implemented!")
+        
+    def GenerateSpectra(self):
+        for s in self.fSpectra.itervalues():
+            if len(s.fAxis) == 1:
+                self.GenerateSpectrum1D(s)
+            elif len(s.fAxis) == 2:
+                self.GenerateSpectrum2D(s)
+            elif len(s.fAxis) == 3:
+                self.GenerateSpectrum3D(s)
+            else:
+                print("Not able to generate spectra with dim > 3!")
+            
+    def GenerateSpectrum1D(self, s):
+        s.fHistogram = ROOT.TH1D(s.fName, s.fName, len(s.fAxis[0].fBins)-1, array('d',s.fAxis[0].fBins))
+        s.fHistogram.GetXaxis().SetTitle(s.fAxis[0].fTitle)
+        s.fHistogram.GetYaxis().SetTitle("counts")
+        s.fHistogram.Sumw2()
+        for binSetName in s.fBins:
+            for bin in self.fBinSet.fBins[binSetName]:
+                if bin.fMassFitter is None:
+                    print("The bin printed below does not have a mass fitter!")
+                    bin.Print()
+                    continue
+                xbin = s.fHistogram.GetXaxis().FindBin(bin.GetBinCenter(s.fAxis[0].fName))
+                s.fHistogram.SetBinContent(xbin, bin.fMassFitter.GetSignal())
+                s.fHistogram.SetBinError(xbin, bin.fMassFitter.GetSignalError())
+                
+    def GenerateSpectrum2D(self, s):
+        s.fHistogram = ROOT.TH2D(s.fName, s.fName, len(s.fAxis[0].fBins)-1, array('d',s.fAxis[0].fBins), len(s.fAxis[1].fBins)-1, array('d',s.fAxis[1].fBins))
+        s.fHistogram.GetXaxis().SetTitle(s.fAxis[0].fTitle)
+        s.fHistogram.GetYaxis().SetTitle(s.fAxis[1].fTitle)
+        s.fHistogram.GetZaxis().SetTitle("counts")
+        s.fHistogram.Sumw2()
+        for binSetName in s.fBins:
+            for bin in self.fBinSet.fBins[binSetName]:
+                if bin.fMassFitter is None:
+                    print("The bin printed below does not have a mass fitter!")
+                    bin.Print()
+                    continue
+                xbin = s.fHistogram.GetXaxis().FindBin(bin.GetBinCenter(s.fAxis[0].fName))
+                ybin = s.fHistogram.GetYaxis().FindBin(bin.GetBinCenter(s.fAxis[1].fName))
+                s.fHistogram.SetBinContent(xbin, ybin, bin.fMassFitter.GetSignal())
+                s.fHistogram.SetBinError(xbin, ybin, bin.fMassFitter.GetSignalError())
+                
+    def GenerateSpectrum3D(self, s):
+        print("GenerateSpectrum3D not implemented!")
+                
     def DrawFitResutls(self,bin):
-        if not bin.fMassFitter:
+        if bin.fMassFitter is None:
             return
         
         bin.fMassFitter.Draw("same");
 
-        paveSig = ROOT.TPaveText(0.165, 0.710, 0.490, 0.925, "NB NDC")
+        paveSig = ROOT.TPaveText(0.165, 0.710, 0.490, 0.92, "NB NDC")
         globalList.append(paveSig)
         paveSig.SetBorderSize(0)
         paveSig.SetFillStyle(0)
@@ -169,7 +276,7 @@ class DMesonJetAnalysis:
          
         eng = DMesonJetAnalysisEngine(config["trigger"], config["d_meson"], 
                                      binSet, config["n_mass_bins"], config["min_mass"], config["max_mass"],
-                                     config["jets"], self.fProjector)
+                                     config["jets"], config["spectra"], self.fProjector)
         self.fAnalysisEngine.append(eng)
         eng.Start()
         
