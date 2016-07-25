273a662d5b39a610640c4b0c7187c34d749b7ec5
saiola <salvatore.aiola@cern.ch>
2016-07-25 15:36:32 -0400

    Fix histogram name and axis

--- DMesonJetAnalysis/DMesonJetBase.py
+++ DMesonJetAnalysis/DMesonJetBase.py
@@ -207,14 +207,12 @@ class DetectorResponse:
             self.fEfficiency1D.append(eff)
 
     def GeneratePartialMultiEfficiencyForBin(self, axis, axisProj, bin, truth, recoTruth):
-        hname = truth.GetName().replace("Truth", "Efficiency")
-
+        #hname = truth.GetName().replace("Truth", "Efficiency")
         #eff = ROOT.TGraphAsymmErrors(recoTruthProj, truthProj, "b(1,1) mode")
         #eff.SetName("{0}_{1}".format(binName, name))
 
-        eff = self.GenerateTruth([axis], hname)
+        eff = self.GenerateTruth([axis], "Efficiency")
         eff.SetTitle(truth.GetTitle())
-        eff.GetXaxis().SetTitle(axisProj.GetTitle())
         eff.GetYaxis().SetTitle("Efficiency")
         eff.Divide(recoTruth, truth)
 
