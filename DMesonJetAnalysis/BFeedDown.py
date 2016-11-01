78db86e4075dad4ac471200c9ee4f7e37ddbe28a
saiola <salvatore.aiola@cern.ch>
2016-11-01 22:50:21 +0100

    Fix file names

--- DMesonJetAnalysis/BFeedDown.py
+++ DMesonJetAnalysis/BFeedDown.py
@@ -27,23 +27,23 @@ def main():
         for obj in r:
             globalList.append(obj)
             if isinstance(obj, ROOT.TCanvas):
-                obj.SaveAs("{0}/FastSimAnalysis_{1}_local_{2}.pdf".format(path, gen, obj.GetName()))
+                obj.SaveAs("{0}/FastSimAnalysis_local_{1}.pdf".format(path, obj.GetName()))
         r = DMesonJetUtils.CompareSpectra(charm_d, [beauty_d], "BFeedDown_D_{0}".format(gen))
         for obj in r:
             globalList.append(obj)
             if isinstance(obj, ROOT.TCanvas):
-                obj.SaveAs("{0}/FastSimAnalysis_{1}_local_{2}.pdf".format(path, gen, obj.GetName()))
+                obj.SaveAs("{0}/FastSimAnalysis_local_{1}.pdf".format(path, obj.GetName()))
 
 def GetSpectrum(filename):
     file = ROOT.TFile(filename)
     jet_list = file.Get("D0_MCTruth_D_Tagged_Jet_Pt_Spectrum")
     jet_h = jet_list.FindObject("D0_MCTruth_D_Tagged_Jet_Pt_Spectrum")
     jet_h.Scale(1, "width")
-    jet_h.GetYaxis().SetTitle("#frac{d#sigma}{d#it{p}_{T}} (mb (GeV/#it{c})^{-1})")
+    jet_h.GetYaxis().SetTitle("#frac{d#sigma}{d#it{p}_{T}} [mb (GeV/#it{c})^{-1}]")
     d_list = file.Get("D0_MCTruth_D_Pt_Spectrum")
     d_h = d_list.FindObject("D0_MCTruth_D_Pt_Spectrum")
     d_h.Scale(1, "width")
-    d_h.GetYaxis().SetTitle("#frac{d#sigma}{d#it{p}_{T}} (mb (GeV/#it{c})^{-1})")
+    d_h.GetYaxis().SetTitle("#frac{d#sigma}{d#it{p}_{T}} [mb (GeV/#it{c})^{-1}]")
     return jet_h, d_h
 
 if __name__ == '__main__':
