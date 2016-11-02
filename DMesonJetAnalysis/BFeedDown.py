440df0d7ecc23d7ab7277d545d46d3b3413dc89d
saiola <salvatore.aiola@cern.ch>
2016-11-02 16:15:19 +0100

    B feed down

--- DMesonJetAnalysis/BFeedDown.py
+++ DMesonJetAnalysis/BFeedDown.py
@@ -11,40 +11,32 @@ globalList = []
 
 def main():
     ROOT.TH1.AddDirectory(False)
+    ROOT.gStyle.SetOptTitle(0)
     path = "/Users/sa639/Documents/Work/ALICE/alice-yale-hfjet/fastSimulation"
-    generators = ["pythia", "powheg"]
+    generators = ["powheg"]
+    spectra = ["D0_MCTruth_D_Tagged_Jet_Pt_Spectrum", "D0_MCTruth_D_Pt_Spectrum", "D0_MCTruth_D_Tagged_Jet_Pt_Spectrum_PtD_2", "D0_MCTruth_D_Pt_Spectrum_JetPt_5"]
     
     for gen in generators:
-        charm_filename = "{0}/FastSimAnalysis_{1}_charm_local.root".format(path, gen)
-        beauty_filename = "{0}/FastSimAnalysis_{1}_beauty_local.root".format(path, gen)
-        charm_jet, charm_d = GetSpectrum(charm_filename)
-        charm_jet.SetTitle("c #rightarrow D^{0}-jet")
-        charm_d.SetTitle("c #rightarrow D^{0}")
-        beauty_jet, beauty_d = GetSpectrum(beauty_filename)
-        beauty_jet.SetTitle("b #rightarrow D^{0}-jet")
-        beauty_d.SetTitle("b #rightarrow D^{0}")
-        r = DMesonJetUtils.CompareSpectra(charm_jet, [beauty_jet], "BFeedDown_Jet_{0}".format(gen))
-        for obj in r:
-            globalList.append(obj)
-            if isinstance(obj, ROOT.TCanvas):
-                obj.SaveAs("{0}/FastSimAnalysis_local_{1}.pdf".format(path, obj.GetName()))
-        r = DMesonJetUtils.CompareSpectra(charm_d, [beauty_d], "BFeedDown_D_{0}".format(gen))
-        for obj in r:
-            globalList.append(obj)
-            if isinstance(obj, ROOT.TCanvas):
-                obj.SaveAs("{0}/FastSimAnalysis_local_{1}.pdf".format(path, obj.GetName()))
+        charm_filename = "{0}/FastSimAnalysis_{1}_charm_1478079449_local.root".format(path, gen)
+        beauty_filename = "{0}/FastSimAnalysis_{1}_beauty_1478079771_local.root".format(path, gen)
+        for spectrum in spectra:
+            charm = GetSpectrum(charm_filename, spectrum)
+            charm.SetTitle("c #rightarrow D^{0}")
+            beauty = GetSpectrum(beauty_filename, spectrum)
+            beauty.SetTitle("b #rightarrow D^{0}")
+            r = DMesonJetUtils.CompareSpectra(charm, [beauty], "BFeedDown_{0}_{1}".format(gen, spectrum))
+            for obj in r:
+                globalList.append(obj)
+                if isinstance(obj, ROOT.TCanvas):
+                    obj.SaveAs("{0}/FastSimAnalysis_local_{1}.pdf".format(path, obj.GetName()))
 
-def GetSpectrum(filename):
+def GetSpectrum(filename, spectrum):
     file = ROOT.TFile(filename)
-    jet_list = file.Get("D0_MCTruth_D_Tagged_Jet_Pt_Spectrum")
-    jet_h = jet_list.FindObject("D0_MCTruth_D_Tagged_Jet_Pt_Spectrum")
-    jet_h.Scale(1, "width")
-    jet_h.GetYaxis().SetTitle("#frac{d#sigma}{d#it{p}_{T}} [mb (GeV/#it{c})^{-1}]")
-    d_list = file.Get("D0_MCTruth_D_Pt_Spectrum")
-    d_h = d_list.FindObject("D0_MCTruth_D_Pt_Spectrum")
-    d_h.Scale(1, "width")
-    d_h.GetYaxis().SetTitle("#frac{d#sigma}{d#it{p}_{T}} [mb (GeV/#it{c})^{-1}]")
-    return jet_h, d_h
+    rlist = file.Get(spectrum)
+    h = rlist.FindObject(spectrum)
+    h.Scale(1, "width")
+    h.GetYaxis().SetTitle("#frac{d#sigma}{d#it{p}_{T}} [mb (GeV/#it{c})^{-1}]")
+    return h
 
 if __name__ == '__main__':
     main()
