c564a4a79cf182e67ce5c3cc8cf2e24ad42b4e59
saiola <salvatore.aiola@cern.ch>
2016-11-11 16:33:28 +0100

    Simplify analysis of fats sim data

--- DMesonJetAnalysis/runDataAnalysis.py
+++ DMesonJetAnalysis/runDataAnalysis.py
@@ -11,7 +11,7 @@ import ROOT
 
 globalList = []
 
-def main(config, maxEvents, format):
+def main(config, maxEvents, format, gen, proc, ts):
     
     ROOT.TH1.AddDirectory(False)
     ROOT.gStyle.SetOptTitle(False)
@@ -20,15 +20,29 @@ def main(config, maxEvents, format):
     subprocess.call("make")
     ROOT.gSystem.Load("MassFitter.so")
 
-    ana = DMesonJetAnalysis.DMesonJetAnalysis(config["name"])
-    projector = DMesonJetProjectors.DMesonJetDataProjector(config["input_path"], config["train"], config["file_name"], config["task_name"], maxEvents)
+    suffix = "{0}_{1}_{2}".format(gen, proc, ts)
+
+    name = config["name"]
+    if "{0}" in name:
+        name = name.format(suffix)
+
+    file_name = config["file_name"]
+    if "{0}" in file_name:
+        file_name = file_name.format(suffix)
+
+    input_path = config["input_path"]
+    if "{0}" in input_path:
+        input_path = input_path.format(suffix)
+
+    ana = DMesonJetAnalysis.DMesonJetAnalysis(name)
+    projector = DMesonJetProjectors.DMesonJetDataProjector(input_path, config["train"], file_name, config["task_name"], maxEvents)
     ana.SetProjector(projector)
 
     for anaConfig in config["analysis"]:
         ana.StartAnalysis(config["figure_title"], config["collision_system"], anaConfig)
 
-    ana.SaveRootFile("{0}/{1}".format(config["input_path"], config["train"]))
-    ana.SavePlots("{0}/{1}".format(config["input_path"], config["train"]), format)
+    ana.SaveRootFile("{0}/{1}".format(input_path, config["train"]))
+    ana.SavePlots("{0}/{1}".format(input_path, config["train"]), format)
 
 if __name__ == '__main__':
     
@@ -39,13 +53,18 @@ if __name__ == '__main__':
                         default=-1, type=int)
     parser.add_argument('--format', metavar='pdf',
                         default="pdf")
+    parser.add_argument('--gen', metavar='GEN',
+                        default="powheg")
+    parser.add_argument('--proc', metavar='PROC',
+                        default="charm")
+    parser.add_argument('--ts', metavar='TS',
+                        default="local")
     args = parser.parse_args()
 
     f = open(args.yaml, 'r')
     config = yaml.load(f)
     f.close()
 
-    main(config, args.events, args.format)
+    main(config, args.events, args.format, args.gen, args.proc, args.ts)
     
     IPython.embed()
-    
