37dfdd9ce6d7f9a628c907198d9c261be8958be4
saiola <salvatore.aiola@cern.ch>
2016-12-03 10:34:33 +0100

    Implementation of FD correction almost complete

--- DMesonJetAnalysis/DMesonJetFDCorrection.py
+++ DMesonJetAnalysis/DMesonJetFDCorrection.py
@@ -6,7 +6,9 @@ import ROOT
 import DMesonJetUtils
 
 class DMesonJetFDCorrection:
-    def __init__(self, config=None):
+    def __init__(self, inputPath, config=None):
+        self.fInputPath = inputPath
+        self.fFDHistograms = dict()
         if config:
             self.fTrainName = config["train"]
             self.fGenerator = config["gen"]
@@ -15,8 +17,32 @@ class DMesonJetFDCorrection:
             self.fSudDir = config["subdir"]
             self.fSpectrumName = config["spectrum"]
             self.GetFDcounts = self.GetFDcountsInternal
+            self.LoadFD()
         else:
             self.GetFDcounts = self.GetFDcountsDummy
+
+    def LoadFD(self):
+        filename = "{0}/{1}_{2}_{3}_{4}/{5}/{1}_{2}_{3}_{4}.root".format(
+            self.fInputPath, self.fTrainName, self.fGenerator, self.fProcess, self.fTimeStamp, self.fSubDir)
+        file = ROOT.TFile(filename)
+        if not file or file.IsZombie():
+            print("Could not open file {0}".format(filename))
+            return
+        else:
+            print("File {0} open for FD correction".format(filename))
+        rlist = file.Get(self.fSpectrumName)
+        if not rlist:
+            print("Could not find list {0}".format(self.fSpectrumName))
+            return
+        else:
+            print("List {0} loaded".format(self.fSpectrumName))
+        h = rlist.FindObject(self.fSpectrumName)
+        if not h:
+            print("Could not find histogram {0}".format(self.fSpectrumName))
+            return
+        else:
+            print("Histogram {0} loaded".format(self.fSpectrumName))
+        self.fFDHistogram = h.Clone("FD")
 
     def GetFDcountsDummy(self, jetpt):
         return 0., 0.
@@ -24,16 +50,26 @@ class DMesonJetFDCorrection:
     def GetFDcountsInternal(self, jetpt):
         return 0., 0.
 
-    def GenerateFDhistogram(self, axis, cuts):
-        h = DMesonJetUtils.BuildHistogram(axis, "FD", "counts")
-        if len(axis) == 1:
-            self.Fill1D(h, cuts)
-        elif len(axis) == 2:
-            self.Fill2D(h, cuts)
+    def GetFDhistogram(self, axis):
+        key = "_".join([axis.fName]+axis.fBins)
+        if key in self.fFDHistograms:
+            return self.fFDHistograms[key]
         else:
-            print("Error - FD correction: cannot handle histograms with dimension > 2!")
+            return self.GenerateFDhistogram(axis)
+
+    def GenerateFDhistogram(self, axis):
+        key = "_".join([axis.fName]+axis.fBins)
+        print("Generating FD spectrum {0}".format(key))
+        h = DMesonJetUtils.BuildHistogram(axis, key, "counts")
+        if len(axis) == 1 and axis[0].fName == "jet_pt":
+            self.Fill1D(h)
+        else:
+            print("Error - FD correction: cannot handle histograms with dimension > 1 or different from jet pt!")
+        self.fFDHistograms[key] = h
         return h
 
     def Fill1D(self, h, cuts):
         for xbin in range(1, h.GetNbinsX()+1):
-            pass
+            v,e = self.GetFDcounts(h.GetXaxis().GetBinCenter(xbin))
+            h.SetBinContent(xbin, v)
+            h.SetBinError(xbin, e)
