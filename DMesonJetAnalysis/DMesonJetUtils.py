2f8f03b7fb10a77d738d23a3e7fb09b23f34527a
saiola <salvatore.aiola@cern.ch>
2016-12-21 15:59:27 +0100

    Further binning change, small bug fixes

--- DMesonJetAnalysis/DMesonJetUtils.py
+++ DMesonJetAnalysis/DMesonJetUtils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#python utilities for D Meson jet analysis
+# python utilities for D Meson jet analysis
 
 import os
 import ROOT
@@ -13,12 +13,12 @@ def ConvertDMesonName(dmeson):
         return dmeson
 
 def binom(n, k):
-    return math.factorial(n) / math.factorial(n-k) / math.factorial(k)
+    return math.factorial(n) / math.factorial(n - k) / math.factorial(k)
 
 def GenerateMultiCanvas(cname, n):
     rows = int(math.floor(math.sqrt(n)))
     cols = int(math.ceil(float(n) / rows))
-    c = ROOT.TCanvas(cname, cname, cols*400, rows*400)
+    c = ROOT.TCanvas(cname, cname, cols * 400, rows * 400)
     c.Divide(cols, rows)
     return c
 
@@ -44,9 +44,9 @@ def CompareSpectra(baseline, spectra, comparisonName, opt="", optRatio="", yaxis
     if styles:
         colors = styles["colors"]
         markers = styles["markers"]
-        lines = styles["lines"]        
+        lines = styles["lines"]
     else:
-        colors = [ROOT.kBlack, ROOT.kBlue+2, ROOT.kRed+2, ROOT.kGreen+2, ROOT.kOrange+2, ROOT.kAzure+2, ROOT.kMagenta+2, ROOT.kCyan+2, ROOT.kPink+1, ROOT.kTeal+2]
+        colors = [ROOT.kBlack, ROOT.kBlue + 2, ROOT.kRed + 2, ROOT.kGreen + 2, ROOT.kOrange + 2, ROOT.kAzure + 2, ROOT.kMagenta + 2, ROOT.kCyan + 2, ROOT.kPink + 1, ROOT.kTeal + 2]
         markers = [ROOT.kOpenCircle, ROOT.kFullCircle, ROOT.kFullSquare, ROOT.kFullTriangleUp, ROOT.kFullTriangleDown, ROOT.kFullDiamond, ROOT.kFullStar, ROOT.kStar, ROOT.kOpenCircle]
         lines = [1, 2, 9, 5, 7, 10, 4, 3, 6, 8, 9]
 
@@ -60,9 +60,9 @@ def CompareSpectra(baseline, spectra, comparisonName, opt="", optRatio="", yaxis
             c.SetLogy()
 
         if leg:
-            leg.SetY1(leg.GetY1()-0.04*(len(spectra)+1))
+            leg.SetY1(leg.GetY1() - 0.04 * (len(spectra) + 1))
         else:
-            leg = ROOT.TLegend(0.25, 0.87-0.04*(len(spectra)+1), 0.85, 0.87)
+            leg = ROOT.TLegend(0.25, 0.87 - 0.04 * (len(spectra) + 1), 0.85, 0.87)
             leg.SetName("{0}_legend".format(c.GetName()))
             leg.SetFillStyle(0)
             leg.SetBorderSize(0)
@@ -76,7 +76,7 @@ def CompareSpectra(baseline, spectra, comparisonName, opt="", optRatio="", yaxis
             baseline.SetLineWidth(2)
             baseline.SetLineStyle(lines[0])
             leg.AddEntry(baseline, baseline.GetTitle(), "l")
-        else: 
+        else:
             baseline.SetMarkerColor(colors[0])
             baseline.SetLineColor(colors[0])
             baseline.SetMarkerStyle(markers[0])
@@ -110,9 +110,9 @@ def CompareSpectra(baseline, spectra, comparisonName, opt="", optRatio="", yaxis
             cRatio.SetLogy()
 
         if legRatio:
-            legRatio.SetY1(legRatio.GetY1()-0.04*(len(spectra)))
+            legRatio.SetY1(legRatio.GetY1() - 0.04 * (len(spectra)))
         else:
-            legRatio = ROOT.TLegend(0.25, 0.87-0.04*(len(spectra)+1), 0.85, 0.87)
+            legRatio = ROOT.TLegend(0.25, 0.87 - 0.04 * (len(spectra) + 1), 0.85, 0.87)
             legRatio.SetName("{0}_legend".format(cRatio.GetName()))
             legRatio.SetFillStyle(0)
             legRatio.SetBorderSize(0)
@@ -217,34 +217,34 @@ def DivideNoErrors(ratio, den):
         print("DMesonJetUtils.DivideNoErrors: histograms have different number of bins!")
         return False
 
-    for ibin in range(0, ratio.GetNbinsX()+2):
+    for ibin in range(0, ratio.GetNbinsX() + 2):
         if den.GetBinContent(ibin) == 0:
             ratio.SetBinContent(ibin, 0)
         else:
-            ratio.SetBinContent(ibin, ratio.GetBinContent(ibin)/den.GetBinContent(ibin))
+            ratio.SetBinContent(ibin, ratio.GetBinContent(ibin) / den.GetBinContent(ibin))
 
     return True
 
 def V2TH1(vect):
-    result = ROOT.TH1D("vect", "vect", len(vect)-2, 1, len(vect)-2)
-    for ibin in range(0, result.GetNbinsX()+2):
+    result = ROOT.TH1D("vect", "vect", len(vect) - 2, 1, len(vect) - 2)
+    for ibin in range(0, result.GetNbinsX() + 2):
         result.SetBinContent(ibin, vect[ibin])
     return result
 
 def BuildHistogram(axis, name, yaxis):
     if len(axis) == 1:
-        hist = ROOT.TH1D(name, name, len(axis[0].fBins)-1, array.array('d',axis[0].fBins))
+        hist = ROOT.TH1D(name, name, len(axis[0].fBins) - 1, array.array('d', axis[0].fBins))
         hist.GetXaxis().SetTitle(axis[0].GetTitle())
         hist.GetYaxis().SetTitle(yaxis)
         hist.Sumw2()
     elif len(axis) == 2:
-        hist = ROOT.TH2D(name, name, len(axis[0].fBins)-1, array.array('d',axis[0].fBins), len(axis[1].fBins)-1, array.array('d',axis[1].fBins))
+        hist = ROOT.TH2D(name, name, len(axis[0].fBins) - 1, array.array('d', axis[0].fBins), len(axis[1].fBins) - 1, array.array('d', axis[1].fBins))
         hist.GetXaxis().SetTitle(axis[0].GetTitle())
         hist.GetYaxis().SetTitle(axis[1].GetTitle())
         hist.GetZaxis().SetTitle(yaxis)
         hist.Sumw2()
     else:
-        hist = ROOT.TH2D(name, name, len(axis[0].fBins)-1, array.array('d',axis[0].fBins), len(axis[1].fBins)-1, array.array('d',axis[1].fBins), len(axis[2].fBins)-1, array.array('d',axis[2].fBins))
+        hist = ROOT.TH2D(name, name, len(axis[0].fBins) - 1, array.array('d', axis[0].fBins), len(axis[1].fBins) - 1, array.array('d', axis[1].fBins), len(axis[2].fBins) - 1, array.array('d', axis[2].fBins))
         hist.GetXaxis().SetTitle(axis[0].GetTitle())
         hist.GetYaxis().SetTitle(axis[1].GetTitle())
         hist.GetZaxis().SetTitle(axis[2].GetTitle())
@@ -252,10 +252,12 @@ def BuildHistogram(axis, name, yaxis):
     return hist
 
 def CompareAxis(axis1, axis2):
-    if axis1.GetNbins() != axis2.GetNbins(): return False
-    for ibin in range(0,axis1.GetNbins()+2):
-        if axis1.GetLowEdge(ibin) != axis2.GetLowEdge(ibin): return False
-    return True 
+    if axis1.GetNbins() < axis2.GetNbins(): return -1
+    elif axis1.GetNbins() > axis2.GetNbins(): return 1
+    for ibin in range(0, axis1.GetNbins() + 2):
+        if axis1.GetBinLowEdge(ibin) > axis2.GetBinLowEdge(ibin): return -1
+        elif axis1.GetBinLowEdge(ibin) < axis2.GetBinLowEdge(ibin): return 1
+    return 0
 
 def Rebin1D(hist, xaxis, warnings=False):
     return Rebin1D_fromBins(hist, hist.GetName(), xaxis.GetNbins(), xaxis.GetXbins().GetArray(), warnings)
@@ -264,17 +266,17 @@ def Rebin1D_fromBins(hist, name, nbinsX, binsX, warnings=False):
     r = ROOT.TH1D(name, name, nbinsX, binsX)
     r.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
     r.GetYaxis().SetTitle(hist.GetYaxis().GetTitle())
-    for xbin in range(0, hist.GetXaxis().GetNbins()+2):
+    for xbin in range(0, hist.GetXaxis().GetNbins() + 2):
         xbinCenter = hist.GetXaxis().GetBinCenter(xbin)
         rxbin = r.GetXaxis().FindBin(xbinCenter)
         binValue = hist.GetBinContent(xbin) + r.GetBinContent(rxbin)
-        binError = math.sqrt(hist.GetBinError(xbin)**2 + r.GetBinError(rxbin)**2)
+        binError = math.sqrt(hist.GetBinError(xbin) ** 2 + r.GetBinError(rxbin) ** 2)
         r.SetBinContent(rxbin, binValue)
         r.SetBinError(rxbin, binError)
         if binValue > 0:
             relErr = binError / binValue
             if relErr > 0.9 and warnings:
-                print("Bin ({0}) has rel stat err = {1}. This is VERY dangerous!".format(xbin,relErr))
+                print("Bin ({0}) has rel stat err = {1}. This is VERY dangerous!".format(xbin, relErr))
     return r
 
 def Rebin2D(hist, xaxis, yaxis, warnings=False):
@@ -284,24 +286,24 @@ def Rebin2D_fromBins(hist, name, nbinsX, binsX, nbinsY, binsY, warnings=False):
     r = ROOT.TH2D(name, name, nbinsX, binsX, nbinsY, binsY)
     r.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
     r.GetYaxis().SetTitle(hist.GetYaxis().GetTitle())
-    for xbin in range(0, hist.GetXaxis().GetNbins()+2):
-        for ybin in range(0, hist.GetYaxis().GetNbins()+2):
+    for xbin in range(0, hist.GetXaxis().GetNbins() + 2):
+        for ybin in range(0, hist.GetYaxis().GetNbins() + 2):
             xbinCenter = hist.GetXaxis().GetBinCenter(xbin)
             ybinCenter = hist.GetYaxis().GetBinCenter(ybin)
             rxbin = r.GetXaxis().FindBin(xbinCenter)
             rybin = r.GetYaxis().FindBin(ybinCenter)
             binValue = hist.GetBinContent(xbin, ybin) + r.GetBinContent(rxbin, rybin)
-            binError = math.sqrt(hist.GetBinError(xbin, ybin)**2 + r.GetBinError(rxbin, rybin)**2)
+            binError = math.sqrt(hist.GetBinError(xbin, ybin) ** 2 + r.GetBinError(rxbin, rybin) ** 2)
             r.SetBinContent(rxbin, rybin, binValue)
             r.SetBinError(rxbin, rybin, binError)
-            
-    for xbin in range(0, r.GetXaxis().GetNbins()+2):
-        for ybin in range(0, r.GetYaxis().GetNbins()+2):
+
+    for xbin in range(0, r.GetXaxis().GetNbins() + 2):
+        for ybin in range(0, r.GetYaxis().GetNbins() + 2):
             binValue = r.GetBinContent(xbin, ybin)
             binError = r.GetBinError(xbin, ybin)
             if binValue > 0:
                 relErr = binError / binValue
                 if relErr > 0.9 and warnings:
-                    print("Bin ({0},{1}) has rel stat err = {2}. This is VERY dangerous!".format(xbin,ybin,relErr))
+                    print("Bin ({0},{1}) has rel stat err = {2}. This is VERY dangerous!".format(xbin, ybin, relErr))
     return r
 
