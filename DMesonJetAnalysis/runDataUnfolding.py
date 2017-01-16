b6fc12f73731c20a9b16d8f647504e6924c4b34b
saiola <salvatore.aiola@cern.ch>
2017-01-16 10:29:14 -0500

    Implementation of a new class MT_Spectrum_Wrapper to make it easier to load the raw yield spectra from the multi-trial analysis

--- DMesonJetAnalysis/runDataUnfolding.py
+++ DMesonJetAnalysis/runDataUnfolding.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#python script to run the D meson jet unfolding on 2010 pp data
+# python script to run the D meson jet unfolding on 2010 pp data
 
 import argparse
 import yaml
@@ -10,36 +10,39 @@ import ROOT
 
 globalList = []
 
-def main(config, format):
-    #subprocess.call("make")
-    #ROOT.gSystem.Load("MassFitter.so")
+def main(config, mt, format):
+    # subprocess.call("make")
+    # ROOT.gSystem.Load("MassFitter.so")
 
     ROOT.TH1.AddDirectory(False)
     ROOT.gStyle.SetOptTitle(False)
     ROOT.gStyle.SetOptStat(0)
 
-    ana = DMesonJetUnfolding.DMesonJetUnfolding(config["name"], config["input_path"], config["data_train"], config["data"], config["response_train"], config["response"])
+    ana = DMesonJetUnfolding.DMesonJetUnfolding(config["name"], config["input_path"], config["data_train"], config["data"], config["response_train"], config["response"], mt)
 
     for anaConfig in config["analysis"]:
         if anaConfig["active"]:
             ana.StartUnfolding(anaConfig, config["efficiency"], config["use_overflow"])
 
-    ana.SaveRootFile("{0}/{1}".format(config["input_path"], config["name"]))
-    ana.SavePlots("{0}/{1}".format(config["input_path"], config["name"]), format)
+    ana.SaveRootFile()
+    ana.SavePlots(format)
 
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser(description='D meson jet unfolding for 2010 pp data.')
     parser.add_argument('yaml', metavar='config.yaml',
                         help='YAML configuration file')
     parser.add_argument('--format', metavar='pdf',
                         default="pdf")
+    parser.add_argument("--mt", action='store_const',
+                        default=False, const=True,
+                        help='Use results from Multi-Trial analysis.')
     args = parser.parse_args()
 
     f = open(args.yaml, 'r')
     config = yaml.load(f)
     f.close()
 
-    main(config, args.format)
-    
+    main(config, args.mt, args.format)
+
     IPython.embed()
