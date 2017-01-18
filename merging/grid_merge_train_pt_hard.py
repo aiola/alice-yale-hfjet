7899814639fe885cf7b6a0496c08a0199a9d7d3c
saiola <salvatore.aiola@cern.ch>
2017-01-18 10:54:40 -0500

    Increase ttl for merging pt hard train results

--- merging/grid_merge_train_pt_hard.py
+++ merging/grid_merge_train_pt_hard.py
@@ -117,7 +117,7 @@ def PtHardBinMerging(LocalPath, Datasets, TrainName, TrainNumbers, MinPtHardBin,
             jdlContent = "# This is the startup script \n\
 Executable = \"{executable}\"; \n\
 # Time after which the job is killed (500 min.) \n\
-TTL = \"7200\"; \n\
+TTL = \"14400\"; \n\
 OutputDir = \"{dest}/{pt_hard}/output\"; \n\
 Output = {{ \n\
 \"AnalysisResults*.root\", \n\
