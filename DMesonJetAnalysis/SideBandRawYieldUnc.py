83d6c1a60a436e1c783ed7b8dcee4f30b16b4f86
saiola <salvatore.aiola@cern.ch>
2017-01-23 10:37:02 -0500

    Add raw yield unc in side-band after summing d pt bins

--- DMesonJetAnalysis/SideBandRawYieldUnc.py
+++ DMesonJetAnalysis/SideBandRawYieldUnc.py
@@ -15,6 +15,34 @@ globalList = []
 input_path = "/Volumes/DATA/ALICE/JetResults"
 ana = "Jets_EMC_pp_823_824_825_826/LHC10_Train823_efficiency"
 
+def SideBandFinalRawYieldUnc():
+    fname = "{0}/{1}/RawYieldUnc_refl_DoubleGaus/DistributionOfFinalYields_SBApproach_Dzero_AfterDbinSum.root".format(input_path, ana)
+    file = ROOT.TFile(fname)
+    canvas = file.Get("cDistr_Dzero_SideBand")
+    histos = []
+    for obj in canvas.GetListOfPrimitives():
+        if isinstance(obj, ROOT.TH1):
+            n = len(histos)
+            h_copy = obj.Clone("var{0}".format(n))
+            h_copy.SetTitle("Variation {0}".format(n))
+            histos.append(h_copy)
+    file.Close()
+    fname = "{0}/{1}/RawYieldUnc_refl_DoubleGaus/TrialExpoFreeS_Dzero_SideBand.root".format(input_path, ana)
+    file = ROOT.TFile(fname)
+    baseline = file.Get("fJetSpectrSBDef")
+    baseline_copy = baseline.Clone("baseline")
+    baseline_copy.SetTitle("TrialExpoFreeS")
+    file.Close()
+    globalList.append(baseline_copy)
+    globalList.extend(histos)
+    cname = "CompareRawYieldUncVariations_AfterDbinSum"
+    comp = DMesonJetCompare.DMesonJetCompare(cname)
+    comp.fOptRatio = "hist"
+    r = comp.CompareSpectra(baseline_copy, histos)
+    for obj in r:
+        if not obj in globalList:
+            globalList.append(obj)
+
 def SideBandRawYieldUnc():
     for ibin in range(0, 9):
         fname = "{0}/{1}/RawYieldUnc_refl_DoubleGaus/DistributionOfFinalYields_SBApproach_Dzero_SideBand_{2}.root".format(input_path, ana, ibin)
@@ -79,6 +107,7 @@ def main():
 
     SideBandRawYieldUnc()
     SideBandRawYieldReflUnc()
+    SideBandFinalRawYieldUnc()
 
     for obj in globalList:
         if isinstance(obj, ROOT.TCanvas):
