ee8fd3e279b8bd497c0fb1e537352a52a4f5d63c
saiola <salvatore.aiola@cern.ch>
2017-01-23 14:42:52 -0500

    Add internal and QM17 B feed-down plot

--- DMesonJetAnalysis/BFeedDown_QM17.py
+++ DMesonJetAnalysis/BFeedDown_QM17.py
@@ -20,24 +20,25 @@ def GetBFeedDownSpectra():
     loader.fKinematicCuts = "DPt_30"
     loader.fRawYieldMethod = "SideBand"
     h = loader.GetDefaultSpectrumFromMultiTrial(False)
+    hFDsub = loader.GetDefaultSpectrumFromMultiTrial(True)
     hFD = loader.GetFDCorrection()
     hFD_up = loader.GetFDCorrection(1)
     hFD_down = loader.GetFDCorrection(-1)
-    return h, hFD, hFD_up, hFD_down
+    return h, hFDsub, hFD, hFD_up, hFD_down
 
 def PlotBFeedDown():
-    h, hFD, hFD_up, hFD_down = GetBFeedDownSpectra()
+    h, hFDsub, hFD, hFD_up, hFD_down = GetBFeedDownSpectra()
 
-    cname = "BFeedDown_QM17"
-    canvas = ROOT.TCanvas(cname, cname, 700, 700)
-    globalList.append(canvas)
-    canvas.Divide(1, 2)
-    padMain = canvas.cd(1)
+    cname = "BFeedDown_Internal"
+    canvas1 = ROOT.TCanvas(cname, cname, 700, 700)
+    globalList.append(canvas1)
+    canvas1.Divide(1, 2)
+    padMain = canvas1.cd(1)
     padMain.SetPad(0, 0.35, 1, 1)
     padMain.SetBottomMargin(0)
     padMain.SetLeftMargin(0.15)
     padMain.SetTicks(1, 1)
-    padRatio = canvas.cd(2)
+    padRatio = canvas1.cd(2)
     padRatio.SetPad(0, 0., 1, 0.35)
     padRatio.SetTopMargin(0)
     padRatio.SetBottomMargin(0.27)
@@ -48,7 +49,7 @@ def PlotBFeedDown():
     padMain.SetLogy()
     h_axis = h.DrawCopy("axis")
     globalList.append(h_axis)
-    h_axis.GetYaxis().SetTitle("yield (arb. units)")
+    h_axis.GetYaxis().SetTitle("yield (counts #times efficiency)")
     h_axis.GetYaxis().SetRangeUser(5e1, 3e4)
     h_axis.GetYaxis().SetTitleFont(43)
     h_axis.GetYaxis().SetTitleSize(26)
@@ -61,6 +62,12 @@ def PlotBFeedDown():
     h_copy.SetMarkerColor(ROOT.kBlue + 2)
     h_copy.SetMarkerStyle(ROOT.kFullCircle)
     globalList.append(h_copy)
+
+    hFDsub_copy = hFDsub.DrawCopy("same p")
+    hFDsub_copy.SetLineColor(ROOT.kGreen + 2)
+    hFDsub_copy.SetMarkerColor(ROOT.kGreen + 2)
+    hFDsub_copy.SetMarkerStyle(ROOT.kFullSquare)
+    globalList.append(hFDsub_copy)
 
     hFD_copy = hFD.DrawCopy("same p")
     globalList.append(hFD_copy)
@@ -84,6 +91,11 @@ def PlotBFeedDown():
     ratio.Divide(h_copy)
     globalList.append(ratio)
 
+    ratio_FDsub = hFDsub_copy.DrawCopy("same hist")
+    ratio_FDsub.SetLineWidth(2)
+    ratio_FDsub.Divide(h_copy)
+    globalList.append(ratio_FDsub)
+
     ratio_up = hFD_up_copy.DrawCopy("same hist l")
     globalList.append(ratio_up)
     ratio_up.Divide(h_copy)
@@ -92,7 +104,7 @@ def PlotBFeedDown():
     globalList.append(ratio_up)
     ratio_down.Divide(h_copy)
 
-    ratio.GetYaxis().SetTitle("FD Fraction")
+    ratio.GetYaxis().SetTitle("ratio")
     ratio.GetXaxis().SetTitle("#it{p}_{T,ch jet} (GeV/#it{c})")
     ratio.GetXaxis().SetTitleFont(43)
     ratio.GetXaxis().SetTitleSize(26)
@@ -104,10 +116,10 @@ def PlotBFeedDown():
     ratio.GetYaxis().SetLabelSize(22)
     ratio.GetYaxis().SetTitleOffset(1.4)
     ratio.GetXaxis().SetTitleOffset(2.9)
-    ratio.GetYaxis().SetRangeUser(0, 0.49)
+    ratio.GetYaxis().SetRangeUser(0, 0.99)
 
     padMain.cd()
-    leg1 = ROOT.TLegend(0.42, 0.52, 0.88, 0.70, "", "NB NDC")
+    leg1 = ROOT.TLegend(0.42, 0.60, 0.88, 0.80, "", "NB NDC")
     globalList.append(leg1)
     leg1.SetBorderSize(0)
     leg1.SetFillStyle(0)
@@ -116,19 +128,70 @@ def PlotBFeedDown():
     leg1.SetTextAlign(13)
     leg1.SetMargin(0.2)
     leg1.AddEntry(h_copy, "Uncorrected D^{0}-Jet Yield", "pe")
+    leg1.AddEntry(hFDsub_copy, "FD Subtracted Yield", "pe")
     leg1.AddEntry(hFD_copy, "B Feed-Down (POWHEG)", "pe")
     leg1.AddEntry(ratio_down, "Systematic Uncertainty", "l")
     leg1.Draw()
 
-    return canvas
+    canvas2 = ROOT.TCanvas("BFeedDown_QM17", "BFeedDown_QM17")
+    globalList.append(canvas2)
+    canvas2.SetTicks(1, 1)
+    canvas2.SetLeftMargin(0.13)
+    canvas2.SetRightMargin(0.05)
+    canvas2.SetTopMargin(0.05)
+    canvas2.SetBottomMargin(0.15)
+    canvas2.cd()
+
+    ratio2 = hFD_copy.DrawCopy("p")
+    ratio2.Divide(h_copy)
+    globalList.append(ratio2)
+
+    ratio2.GetYaxis().SetTitle("B Feed-Down Fraction")
+    ratio2.GetXaxis().SetTitle("#it{p}_{T,ch jet} (GeV/#it{c})")
+    ratio2.GetXaxis().SetTitleFont(43)
+    ratio2.GetXaxis().SetTitleSize(26)
+    ratio2.GetXaxis().SetLabelFont(43)
+    ratio2.GetXaxis().SetLabelSize(22)
+    ratio2.GetYaxis().SetTitleFont(43)
+    ratio2.GetYaxis().SetTitleSize(26)
+    ratio2.GetYaxis().SetLabelFont(43)
+    ratio2.GetYaxis().SetLabelSize(22)
+    ratio2.GetYaxis().SetTitleOffset(0.9)
+    ratio2.GetYaxis().SetRangeUser(0, 0.59)
+
+    ratio2_up = hFD_up_copy.DrawCopy("same hist l")
+    globalList.append(ratio2_up)
+    ratio2_up.Divide(h_copy)
+
+    ratio2_down = hFD_down_copy.DrawCopy("same hist l")
+    globalList.append(ratio2_down)
+    ratio2_down.Divide(h_copy)
+
+    paveALICE = ROOT.TPaveText(0.17, 0.61, 0.56, 0.93, "NB NDC")
+    globalList.append(paveALICE)
+    paveALICE.SetBorderSize(0)
+    paveALICE.SetFillStyle(0)
+    paveALICE.SetTextFont(43)
+    paveALICE.SetTextSize(20)
+    paveALICE.SetTextAlign(13)
+    paveALICE.AddText("ALICE Preliminary, pp, #sqrt{#it{s}} = 7 TeV")
+    paveALICE.AddText("Charged Jets, Anti-#it{k}_{T}, #it{R}=0.4, |#eta_{jet}| < 0.5")
+    paveALICE.AddText("with D^{0} #rightarrow K^{-}#pi^{+} and c.c., #it{p}_{T,D} > 3 GeV/#it{c}")
+    paveALICE.AddText("Raw B Feed-Down Fraction")
+    paveALICE.AddText("Not corrected for reconstruction efficiency and")
+    paveALICE.AddText("jet momentum resolution")
+    paveALICE.Draw()
+
+    return canvas1, canvas2
 
 def main():
     ROOT.TH1.AddDirectory(False)
     ROOT.gStyle.SetOptTitle(0)
     ROOT.gStyle.SetOptStat(0)
 
-    canvas = PlotBFeedDown()
-    canvas.SaveAs("{0}/BFeedDown_QM17.pdf".format(input_path))
+    canvas1, canvas2 = PlotBFeedDown()
+    canvas1.SaveAs("{0}/BFeedDown_InternalPlot.pdf".format(input_path))
+    canvas2.SaveAs("{0}/BFeedDown_QM17.pdf".format(input_path))
 
 if __name__ == '__main__':
     main()
