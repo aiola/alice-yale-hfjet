3fd0592d1749680d371bc5faf357e26b26473184
Salvatore Aiola <salvatore.aiola@cern.ch>
2017-04-05 19:55:40 -0400

    scripts to reset/clean the lego train folder

--- lego/reset_lego.sh
+++ lego/reset_lego.sh
@@ -2,21 +2,15 @@
 
 rm *.root
 rm *.xml
-rm MLTrainDefinition.cfg
 rm lego_train*
 rm std*
 rm *.log
-rm globalvariables.C
-rm handlers.C
-rm env.sh
 rm outputs_valid
 rm extract_syswatch.out
-rm generator_customization.C
 rm mergeNoFile
 rm syswatch.png
 rm syswatch.stats
 rm test_finished
 rm test_started
-rm useruserQAconfiguration.yaml
 rm validated
 rm gdb-backtrace*
