9de7b0a21cda13921bdf6b927af5790c1a034b04
Salvatore Aiola <salvatore.aiola@cern.ch>
2017-10-20 17:46:13 +0200

    Add option to combine topo analysis from different trains

--- DMesonJetAnalysis/runTopoAnalysis.py
+++ DMesonJetAnalysis/runTopoAnalysis.py
@@ -16,34 +16,38 @@ globalList = []
 # To mimic ROOT5 behavior
 if ROOT.gROOT.GetVersionInt() >= 60000: ROOT.ROOT.Math.IntegratorOneDimOptions.SetDefaultIntegrator("Gauss")
 
-def main(config, maxEvents, format):
+def main(configs, maxEvents, format):
     ROOT.TH1.AddDirectory(False)
     ROOT.gStyle.SetOptTitle(False)
     ROOT.gStyle.SetOptStat(0)
 
-    name = config["name"]
-    train = config["train"]
-    input_path = config["input_path"]
-    file_name = config["file_name"]
-    output_path = input_path
-
     manager = DMesonJetTopoAnalysis.DMesonJetTopoAnalysisManager("D0")
-
-    projector = DMesonJetProjectors.DMesonJetProjector(input_path, train, file_name, config["task_name"], config["merging_type"], maxEvents)
-    manager.AddAnalysis("Signal", "", projector, "kSignalOnly_D0toKpiCuts_loosest_nopid")
-    manager.AddAnalysis("Background", "", projector, "kBackgroundOnly_D0toKpiCuts_loosest_nopid")
-
     globalList.append(manager)
+
+    name_chain = ""
+
+    for config in configs:
+        name = config["name"]
+        train = config["train"]
+        input_path = config["input_path"]
+        file_name = config["file_name"]
+        output_path = input_path
+        name_chain += name
+
+        projector = DMesonJetProjectors.DMesonJetProjector(input_path, train, file_name, config["task_name"], config["merging_type"], maxEvents)
+
+        for topo_ana in config["topo_studies"]:
+            manager.AddAnalysis(topo_ana["title"], topo_ana["trigger"], projector, topo_ana["d_meson_suffix"])
 
     manager.StartAnalysis()
 
-    manager.SaveRootFile("{0}/{1}".format(output_path, train))
-    manager.SavePlots("{0}/{1}".format(output_path, train), format)
+    manager.SaveRootFile("{0}/{1}".format(output_path, name_chain))
+    manager.SavePlots("{0}/{1}".format(output_path, name_chain), format)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Analysis of the topological cuts.')
-    parser.add_argument('yaml', metavar='config.yaml',
+    parser.add_argument('yaml', metavar='config.yaml', nargs='*',
                         help='YAML configuration file')
     parser.add_argument('--events', metavar='N',
                         default=-1, type=int)
@@ -51,10 +55,14 @@ if __name__ == '__main__':
                         default="pdf")
     args = parser.parse_args()
 
-    f = open(args.yaml, 'r')
-    config = yaml.load(f)
-    f.close()
+    configs = []
 
-    main(config, args.events, args.format)
+    for yaml_filename in args.yaml:
+        f = open(yaml_filename, 'r')
+        config = yaml.load(f)
+        f.close()
+        configs.append(config)
+
+    main(configs, args.events, args.format)
 
     IPython.embed()
