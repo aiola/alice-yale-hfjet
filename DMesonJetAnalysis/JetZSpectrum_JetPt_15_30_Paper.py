1214625fcd7a92b63ed208b033f68b5a0d6c2199
Salvatore Aiola <salvatore.aiola@cern.ch>
2017-12-04 15:08:08 +0100

    Graphic adjustments

--- DMesonJetAnalysis/JetZSpectrum_JetPt_15_30_Paper.py
+++ DMesonJetAnalysis/JetZSpectrum_JetPt_15_30_Paper.py
@@ -168,7 +168,7 @@ def PlotCrossSections(dataStat, dataSyst, theoryStat, theorySystUp, theorySystLo
     leg1.Draw()
 
     padMain.cd()
-    paveALICE = ROOT.TPaveText(0.16, 0.64, 0.55, 0.90, "NB NDC")
+    paveALICE = ROOT.TPaveText(0.16, 0.54, 0.55, 0.90, "NB NDC")
     globalList.append(paveALICE)
     paveALICE.SetBorderSize(0)
     paveALICE.SetFillStyle(0)
@@ -177,8 +177,9 @@ def PlotCrossSections(dataStat, dataSyst, theoryStat, theorySystUp, theorySystLo
     paveALICE.SetTextAlign(13)
     # paveALICE.AddText("ALICE Preliminary")
     paveALICE.AddText("pp, #sqrt{#it{s}} = 7 TeV")
-    paveALICE.AddText("Charged Jets, Anti-#it{k}_{T}, #it{R} = 0.4, |#eta_{jet}| < 0.5")
-    paveALICE.AddText("with D^{0}, #it{p}_{T,D} > 3 GeV/#it{c}")
+    paveALICE.AddText("Charged Jets, Anti-#it{k}_{T}, #it{R} = 0.4, |#eta_{jet}| < 0.5,")
+    paveALICE.AddText("15 < #it{p}_{T,ch jet} < 30 GeV/#it{c}")
+    paveALICE.AddText("with D^{0}, #it{p}_{T,D} > 6 GeV/#it{c}")
     paveALICE.Draw()
 
     padRatio.RedrawAxis("g")
