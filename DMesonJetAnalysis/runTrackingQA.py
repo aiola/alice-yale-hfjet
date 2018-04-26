91c44109c8f0e0a21c58a691303ba2b09aa1b0d5
Salvatore Aiola <salvatore.aiola@cern.ch>
2018-04-26 18:31:36 +0200

    First working version with efficiency plots

--- DMesonJetAnalysis/runTrackingQA.py
+++ DMesonJetAnalysis/runTrackingQA.py
@@ -30,6 +30,8 @@ class Variable:
         self.fMatchedTracksAxis = matched_tracks_axis
         self.fMatchedParticlesAxis = matched_particles_axis
         self.fLogy = logy
+        self.fLogx = False
+        self.fBins = None
 
 
 class Histogram:
@@ -49,6 +51,7 @@ class Histogram:
                 htot.Add(h)
             else:
                 htot = h.Clone(h.GetName()[:-2])
+        htot.SetTitle("Hybrid tracks")
         self.fHistograms["all"] = htot
 
     def NormalizeHistogram(self, events):
@@ -74,6 +77,11 @@ class Histogram:
             l = self.fHistograms
         return [obj for k, obj in l.iteritems() if not k == "all"]
 
+    def RebinIfNeeded(self, h):
+        if not self.fVariable.fBins: return h
+        hrebinned = DMesonJetUtils.Rebin1D_fromBins(h, h.GetName(), len(self.fVariable.fBins) - 1, numpy.array(self.fVariable.fBins, dtype=numpy.float64), True)
+        return hrebinned
+
 
 class TrackHistogram(Histogram):
 
@@ -82,10 +90,10 @@ class TrackHistogram(Histogram):
         for track_type, track_type_title in self.fTrackTypes.iteritems():
             hn.GetAxis(track_type_axis).SetRange(track_type, track_type)
             if self.fMatched:
-                h = hn.Projection(self.fVariable.fMatchedTracksAxis, "A")
+                h = self.RebinIfNeeded(hn.Projection(self.fVariable.fMatchedTracksAxis, "A"))
                 hname = "MatchedTracks{}_{}".format(self.fVariable.fName, track_type)
             else:
-                h = hn.Projection(self.fVariable.fTracksAxis, "A")
+                h = self.RebinIfNeeded(hn.Projection(self.fVariable.fTracksAxis, "A"))
                 hname = "Tracks{}_{}".format(self.fVariable.fName, track_type)
             hn.GetAxis(track_type_axis).SetRange(0, -1)
             xaxis_title = self.fVariable.fTitle
@@ -111,12 +119,12 @@ class ParticleHistogram(Histogram):
         for track_type, track_type_title in track_types.iteritems():
             if self.fMatched:
                 hn.GetAxis(track_type_axis).SetRange(track_type, track_type)
-                h = hn.Projection(self.fVariable.fMatchedParticlesAxis, "A")
+                h = self.RebinIfNeeded(hn.Projection(self.fVariable.fMatchedParticlesAxis, "A"))
                 hn.GetAxis(track_type_axis).SetRange(0, -1)
                 hname = "MatchedParticles{}_{}".format(self.fVariable.fName, track_type)
             else:
                 hn.GetAxis(track_type_axis).SetRange(2, 2)  # findable
-                h = hn.Projection(self.fVariable.fParticlesAxis, "A")
+                h = self.RebinIfNeeded(hn.Projection(self.fVariable.fParticlesAxis, "A"))
                 hn.GetAxis(track_type_axis).SetRange(0, -1)
                 hname = "Particles{}".format(self.fVariable.fName)
             xaxis_title = self.fVariable.fTitle
@@ -132,6 +140,18 @@ class ParticleHistogram(Histogram):
                 self.fHistograms[track_type] = h
 
 
+class EfficiencyHistogram(Histogram):
+
+    def Generate(self, all_histograms, matched_histograms):
+        hall = all_histograms.fHistograms["all"]
+        for track_type, hmatched in matched_histograms.fHistograms.iteritems():
+            eff = hmatched.Clone("{}_Efficiency".format(hmatched.GetName()))
+            eff.Divide(hall)
+            eff.GetYaxis().SetTitle("Efficiency")
+            self.fHistograms[track_type] = eff
+        self.fNormHistograms = self.fHistograms
+
+
 class TrackingQA:
 
     def __init__(self, input_path, train):
@@ -142,7 +162,13 @@ class TrackingQA:
         self.fHnParticlesName = "fParticlesPhysPrim"
         self.fHnMatchedName = "fParticlesMatched"
         self.fVariables = []
-        self.fVariables.append(Variable("Pt", "#it{p}_{T}", "(GeV/#it{c})", True, 0, 0, 3, 0))
+        pt = Variable("Pt", "#it{p}_{T}", "(GeV/#it{c})", True, 0, 0, 3, 0)
+        pt.fBins = [0.15, 0.20, 0.25, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90,
+                    1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0,
+                    3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0,
+                    15, 20, 30, 40]
+        pt.fLogx = True
+        self.fVariables.append(pt)
         self.fVariables.append(Variable("Eta", "#eta", "", True, 1, 1, 4, 1))
         self.fVariables.append(Variable("Phi", "#phi", "", True, 2, 2, 5, 2))
         self.fTrackTypeAxis = 4
@@ -154,11 +180,14 @@ class TrackingQA:
         self.fParticleHistograms = dict()
         self.fMatchedTracksHistograms = dict()
         self.fMatchedParticleHistograms = dict()
+        self.fEfficiencyHistograms = dict()
+        self.fFakeRateHistograms = dict()
         self.fHnTracks = []
         self.fHnParticles = []
         self.fHnMatched = []
         self.fNEvents = []
         self.fTotEvents = 0
+        self.fCanvases = []
 
     def ExtractPeriod(self, fname):
         train_index = fname.find(self.fTrain)
@@ -227,17 +256,63 @@ class TrackingQA:
         self.PlotSpectra("MatchedTracks", self.fMatchedTracksHistograms)
         self.PlotSpectra("MatchedParticles", self.fMatchedParticleHistograms)
 
+        self.PlotEfficiency()
+
     def PlotSpectra(self, name, histograms):
         for var in self.fVariables:
             comp_name = "{}_{}".format(name, var.fName)
             comp = DMesonJetCompare.DMesonJetCompare(comp_name)
             comp.fDoRatioPlot = "logy"
+            comp.fX1LegRatio = 0.15
+            comp.fX2LegRatio = 0.50
             if var.fLogy:
                 comp.fDoSpectraPlot = "logy"
             else:
                 comp.fDoSpectraPlot = "lineary"
-            results = comp.CompareSpectra(histograms[var.fName].GetFullHistogram(), histograms[var.fName].GetPartialHistograms())
-            for r in results: globalList.append(r)
+            baseline = histograms[var.fName].GetFullHistogram()
+            histos = histograms[var.fName].GetPartialHistograms()
+            tot = baseline.Integral()
+            for h in histos:
+                partial = h.Integral() / tot
+                print("Fraction of '{}': {}".format(h.GetTitle(), partial))
+            results = comp.CompareSpectra(baseline, histos)
+            for r in results:
+                if isinstance(r, ROOT.TCanvas):
+                    if var.fLogx: r.SetLogx()
+                    self.fCanvases.append(r)
+                globalList.append(r)
+
+    def PlotEfficiency(self):
+        for var in self.fVariables:
+            comp_name = "Efficiency_{}".format(var.fName)
+            comp = DMesonJetCompare.DMesonJetCompare(comp_name)
+            comp.fDoRatioPlot = False
+            comp.fDoSpectraPlot = "lineary"
+            comp.fX1LegSpectrum = 0.15
+            comp.fX2LegSpectrum = 0.50
+            comp.fLinUpperSpace = 0.4
+            comp.fOptSpectrum = "hist"
+            comp.fOptSpectrumBaseline = "hist"
+            comp.fGridySpectrum = True
+            baseline = self.fEfficiencyHistograms[var.fName].GetFullHistogram()
+            histos = self.fEfficiencyHistograms[var.fName].GetPartialHistograms()
+            results = comp.CompareSpectra(baseline, histos)
+            for r in results:
+                if isinstance(r, ROOT.TCanvas):
+                    if var.fLogx: r.SetLogx()
+                    self.fCanvases.append(r)
+                globalList.append(r)
+
+    def CalculateEfficiency(self):
+        for var in self.fVariables:
+            efficiencies = EfficiencyHistogram(var, True)
+            efficiencies.Generate(self.fParticleHistograms[var.fName], self.fMatchedParticleHistograms[var.fName])
+            self.fEfficiencyHistograms[var.fName] = efficiencies
+
+    def SaveCanvases(self):
+        for c in self.fCanvases:
+            fname = "{}/{}/{}.pdf".format(self.fInputPath, self.fTrain, c.GetName())
+            c.SaveAs(fname)
 
 
 def main(input_path, train):
@@ -249,7 +324,9 @@ def main(input_path, train):
     globalList.append(qa)
     qa.LoadHns()
     qa.ProjectHistograms()
+    qa.CalculateEfficiency()
     qa.Plot()
+    qa.SaveCanvases()
 
 
 if __name__ == '__main__':
