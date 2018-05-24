b86952a0914bf6831a965bb4e9a78fbb0151cae7
Salvatore Aiola <salvatore.aiola@cern.ch>
2018-05-24 16:44:43 +0200

    Load insclusive jet spectrum becomes an external module, add comparison with data in the inclusive jet theory

--- DMesonJetAnalysis/D0vsInclJets_Paper.py
+++ DMesonJetAnalysis/D0vsInclJets_Paper.py
@@ -5,6 +5,7 @@ import math
 import IPython
 import ROOT
 import DMesonJetUtils
+import LoadInclusiveJetSpectrum
 
 globalList = []
 
@@ -72,74 +73,7 @@ def GetD0JetCrossSection():
 
 
 def GetInclJetCrossSection():
-    sigmapp = 73.2  # published x-section in mbarn
-    triggerEff = 62.2 / 73.2  # eff of MB OR trigger to be applied to data
-    bin0CorrData = 0.91  # ratio event after vertexNcontributors cut to evts after phys selection
-
-    fname = "../obusch/outData_spec_Bayes_combPtH_rebinned.root"
-    file = ROOT.TFile(fname)
-    if not file or file.IsZombie():
-        print("Could not open file {}".format(fname))
-        exit(1)
-    hStat = file.Get("hSpecComb")
-    if not hStat:
-        print("Cannot get inclusive cross section with statistical uncertainty!")
-        exit(1)
-    file.Close()
-
-    hStat.Scale(sigmapp * triggerEff)
-    hStat.Scale(bin0CorrData)
-
-    fname = "../obusch/outData_spec_Bayes_combPtH.root"
-    file = ROOT.TFile(fname)
-    if not file or file.IsZombie():
-        print("Could not open file {}".format(fname))
-        exit(1)
-    hStat_orig = file.Get("hSpecComb")
-    if not hStat_orig:
-        print("Cannot get inclusive cross section with statistical uncertainty (not rebinned)!")
-        exit(1)
-    file.Close()
-
-    hStat_orig.Scale(sigmapp * triggerEff)
-    hStat_orig.Scale(bin0CorrData)
-
-    fname = "../obusch/outSys_tot_spec.root"
-    file = ROOT.TFile(fname)
-    if not file or file.IsZombie():
-        print("Could not open file {}".format(fname))
-        exit(1)
-    hSyst_orig = file.Get("grSysErrTotNoUE")
-    if not hSyst_orig:
-        print("Cannot get inclusive cross section with systematic uncertainty!")
-        exit(1)
-    file.Close()
-
-    hSyst = ROOT.TGraphErrors(hStat.GetNbinsX())
-
-    for i in range(1, hStat.GetNbinsX() + 1):
-        x = hStat.GetXaxis().GetBinCenter(i)
-        y = hStat.GetBinContent(i)
-        xerr = hStat.GetXaxis().GetBinWidth(i) / 2
-        print("hStat Bin {}, x in [{}, {}], y = {}".format(i, x - xerr, x + xerr, y))
-        yerr_rel = 0.
-        y_check = 0.
-        for iorig in range(0, hSyst_orig.GetN()):
-            if hSyst_orig.GetX()[iorig] < hStat.GetXaxis().GetBinLowEdge(i): continue
-            if hSyst_orig.GetX()[iorig] > hStat.GetXaxis().GetBinUpEdge(i): break
-            yerr_rel += hSyst_orig.GetErrorY(iorig) * hStat_orig.GetBinContent(iorig + 1) * hStat_orig.GetXaxis().GetBinWidth(iorig + 1)
-            y_check += hStat_orig.GetBinContent(iorig + 1) * hStat_orig.GetXaxis().GetBinWidth(iorig + 1)
-            print("hSyst_orig Bin {}, x = {}, err_y_rel = {}".format(iorig, hSyst_orig.GetX()[iorig], hSyst_orig.GetErrorY(iorig)))
-            print("hStat_orig Bin {}, x = {}, y = {}, err_y_rel = {}".format(iorig, hStat_orig.GetXaxis().GetBinCenter(iorig + 1), hStat_orig.GetBinContent(iorig + 1), hStat_orig.GetBinError(iorig + 1)))
-        yerr_rel /= y_check
-        y_check /= hStat.GetXaxis().GetBinWidth(i)
-        yerr = yerr_rel * y
-        hSyst.SetPoint(i - 1, x, y)
-        hSyst.SetPointError(i - 1, xerr, yerr)
-        print("Bin {} Tot Error rel {}".format(i, yerr_rel))
-        print("Bin {}, tot y {}, check {}\n".format(i, y, y_check))
-
-    return hStat, hSyst
+    return LoadInclusiveJetSpectrum.GetCrossSection()
 
 
 def PlotCrossSections(d0Stat, d0Syst, inclStat, inclSyst, theory_d0Stat, theory_d0Syst, theory_noptcut_d0Stat, theory_inclStat):
