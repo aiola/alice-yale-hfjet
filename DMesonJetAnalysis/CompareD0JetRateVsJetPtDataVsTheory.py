316f221ebe0da5fb954b1cb8a71877805035d8c9
Salvatore Aiola <salvatore.aiola@cern.ch>
2018-06-20 15:04:09 +0200

    Reorganize the code to have separate computation and plotting logic

--- DMesonJetAnalysis/CompareD0JetRateVsJetPtDataVsTheory.py
+++ DMesonJetAnalysis/CompareD0JetRateVsJetPtDataVsTheory.py
@@ -35,7 +35,6 @@ def GetD0JetCrossSection(input_path, file_name):
         exit(1)
     return hStat, hSyst
 
-
 def GetInclJetCrossSection():
     return LoadInclusiveJetSpectrum.GetCrossSection()
 
@@ -46,51 +45,13 @@ def GetInclusiveJetTheoryCrossSectionAll(config):
     return LoadTheoryCrossSections.GetInclusiveJetTheoryCrossSectionAll(config)
 
 def PlotCrossSections(d0jet_stat, d0jet_syst, incl_stat, incl_syst, inclusive_jet_cross_sections, config):
-    cname = "{}_{}".format(config["D0JetRate"]["name_prefix"], config["name"])
-    if "canvas_h" in config:
-        canvas_h = config["canvas_h"]
-    else:
-        canvas_h = 900
-    if "canvas_w" in config:
-        canvas_w = config["canvas_w"]
-    else:
-        canvas_w = 700
-    canvas = ROOT.TCanvas(cname, cname, canvas_w, canvas_h)
-    globalList.append(canvas)
-    canvas.Divide(1, 2)
-    padMain = canvas.cd(1)
-    padMain.SetPad(0, 0.35, 1, 1)
-    padMain.SetBottomMargin(0)
-    padMain.SetLeftMargin(0.15)
-    padMain.SetTicks(1, 1)
-    padRatio = canvas.cd(2)
-    padRatio.SetPad(0, 0., 1, 0.35)
-    padRatio.SetTopMargin(0)
-    padRatio.SetBottomMargin(0.27)
-    padRatio.SetLeftMargin(0.15)
-    padRatio.SetGridy()
-    padRatio.SetTicks(1, 1)
-
-    padMain.cd()
-    if config["logy"]: padMain.SetLogy()
-    h = d0jet_stat.DrawCopy("axis")
-    h.GetYaxis().SetRangeUser(config["D0JetRate"]["miny"], config["D0JetRate"]["maxy"])
-    h.GetYaxis().SetTitleFont(43)
-    h.GetYaxis().SetTitleSize(23)
-    h.GetYaxis().SetLabelFont(43)
-    h.GetYaxis().SetLabelSize(22)
-    h.GetYaxis().SetTitleOffset(1.6)
-    if "y_axis_title" in config:
-        h.GetYaxis().SetTitle(config["y_axis_title"])
-
     d0jet_syst_copy = d0jet_syst.Clone("{0}_copy".format(d0jet_syst.GetName()))
-    d0jet_syst_copy.Draw("2")
     d0jet_syst_copy.SetFillStyle(1001)
     d0jet_syst_copy.SetLineWidth(0)
     d0jet_syst_copy.SetFillColor(ROOT.kGray)
     globalList.append(d0jet_syst_copy)
 
-    d0jet_stat_copy = d0jet_stat.DrawCopy("same p e0 x0")
+    d0jet_stat_copy = d0jet_stat.Clone("d0jet_stat_copy")
     d0jet_stat_copy.SetLineColor(ROOT.kRed + 2)
     d0jet_stat_copy.SetMarkerColor(ROOT.kRed + 2)
     d0jet_stat_copy.SetMarkerStyle(ROOT.kFullCircle)
@@ -99,42 +60,41 @@ def PlotCrossSections(d0jet_stat, d0jet_syst, incl_stat, incl_syst, inclusive_je
 
     incl_syst_copy = incl_syst.Clone("incl_syst_copy")
     globalList.append(incl_syst_copy)
-    incl_syst_copy.Draw("2")
     incl_syst_copy.SetFillStyle(1001)
     incl_syst_copy.SetLineWidth(0)
     incl_syst_copy.SetFillColor(ROOT.kGray)
 
-    incl_stat_copy = incl_stat.DrawCopy("same p e0 x0")
+    incl_stat_copy = incl_stat.Clone("incl_stat_copy")
     globalList.append(incl_stat_copy)
     incl_stat_copy.SetLineColor(ROOT.kBlack)
     incl_stat_copy.SetMarkerColor(ROOT.kBlack)
     incl_stat_copy.SetMarkerStyle(ROOT.kFullSquare)
     incl_stat_copy.SetMarkerSize(1.2)
 
-    padRatio.cd()
+    for t in inclusive_jet_cross_sections.itervalues():
+        incl_h = t["histogram"].Clone("_".join([t["gen"], t["proc"]]))
+        globalList.append(incl_h)
+        incl_h.SetLineColor(t["color"])
+        incl_h.SetLineStyle(t["line"])
+        incl_h.SetLineWidth(2)
+        incl_h.SetMarkerColor(t["color"])
+        t["histogram_plot"] = incl_h
 
-    hRatio = d0jet_stat_copy.DrawCopy("axis")
-    hRatio.GetYaxis().SetTitle("#frac{#it{R}(#it{p}_{T,jet}^{ch})}{#Delta#it{p}_{T,jet}^{ch}} (GeV/#it{c})^{-1}")
-    hRatio.GetXaxis().SetTitleFont(43)
-    hRatio.GetXaxis().SetTitleSize(23)
-    hRatio.GetXaxis().SetLabelFont(43)
-    hRatio.GetXaxis().SetLabelSize(22)
-    hRatio.GetYaxis().SetTitleFont(43)
-    hRatio.GetYaxis().SetTitleSize(23)
-    hRatio.GetYaxis().SetLabelFont(43)
-    hRatio.GetYaxis().SetLabelSize(22)
-    hRatio.GetYaxis().SetTitleOffset(1.9)
-    hRatio.GetXaxis().SetTitleOffset(2.9)
-    hRatio.GetYaxis().SetRangeUser(config["D0JetRate"]["minr"], config["D0JetRate"]["maxr"])
-    hRatio.GetYaxis().SetNdivisions(509)
-    if "x_axis_title" in config:
-        hRatio.GetXaxis().SetTitle(config["x_axis_title"])
+    for t in config["theory"]:
+        if not t["active"]: continue
+        if not t["inclusive"]: continue
+        d0jet_h = t["histogram"].Clone("_".join([t["gen"], t["proc"]]))
+        globalList.append(d0jet_h)
+        d0jet_h.SetLineColor(t["color"])
+        d0jet_h.SetLineStyle(t["line"])
+        d0jet_h.SetLineWidth(2)
+        d0jet_h.SetMarkerColor(t["color"])
+        t["histogram_plot"] = d0jet_h
 
     ratioSyst = d0jet_syst_copy.Clone("ratioSyst")
     globalList.append(ratioSyst)
-    ratioSyst.Draw("2")
 
-    ratioStat = d0jet_stat_copy.DrawCopy("same p e0 x0")
+    ratioStat = d0jet_stat_copy.Clone("ratioStat")
     globalList.append(ratioStat)
 
     xsec_incl_tot = 0.0
@@ -202,43 +162,110 @@ def PlotCrossSections(d0jet_stat, d0jet_syst, incl_stat, incl_syst, inclusive_je
     print("Average ratio in full range: {} +/- {} (stat) +/- {} (syst)".format(avg_tot, stat_avg_tot, syst_avg_tot))
     print("Average ratio in flat region (pt > 8 GeV/c): {} +/- {} (stat) +/- {} (syst)".format(avg_flat, stat_avg_flat, syst_avg_flat))
 
+    for t in config["theory"]:
+        if not t["active"]: continue
+        if not t["inclusive"]: continue
+        incl_h = t["inclusive"]["histogram_plot"]
+        d0jet_h = t["histogram_plot"]
+        hratio = d0jet_h.Clone("_".join([t["gen"], t["proc"], "over", t["inclusive"]["gen"], t["inclusive"]["proc"]]))
+        hratio.Divide(incl_h)
+        t["ratio_histogram"] = hratio
+
+    # Done with all the preparations, now plotting
+
+    hAxis = ROOT.TH1I("axis", "axis", 1000, d0jet_stat_copy.GetXaxis().GetBinLowEdge(1), d0jet_stat_copy.GetXaxis().GetBinUpEdge(d0jet_stat_copy.GetXaxis().GetNbins()))
+    hAxis.GetYaxis().SetTitleFont(43)
+    hAxis.GetYaxis().SetTitleSize(23)
+    hAxis.GetYaxis().SetLabelFont(43)
+    hAxis.GetYaxis().SetLabelSize(22)
+    hAxis.GetYaxis().SetTitleOffset(1.6)
+    hAxis.GetYaxis().SetRangeUser(config["D0JetRate"]["miny"], config["D0JetRate"]["maxy"])
+    if "y_axis_title" in config:
+        hAxis.GetYaxis().SetTitle(config["y_axis_title"])
+
+    hAxisRatio = ROOT.TH1I("axis", "axis", 1000, d0jet_stat_copy.GetXaxis().GetBinLowEdge(1), d0jet_stat_copy.GetXaxis().GetBinUpEdge(d0jet_stat_copy.GetXaxis().GetNbins()))
+    hAxisRatio.GetYaxis().SetTitle("#frac{#it{R}(#it{p}_{T,jet}^{ch})}{#Delta#it{p}_{T,jet}^{ch}} (GeV/#it{c})^{-1}")
+    hAxisRatio.GetXaxis().SetTitleFont(43)
+    hAxisRatio.GetXaxis().SetTitleSize(23)
+    hAxisRatio.GetXaxis().SetLabelFont(43)
+    hAxisRatio.GetXaxis().SetLabelSize(22)
+    hAxisRatio.GetYaxis().SetTitleFont(43)
+    hAxisRatio.GetYaxis().SetTitleSize(23)
+    hAxisRatio.GetYaxis().SetLabelFont(43)
+    hAxisRatio.GetYaxis().SetLabelSize(22)
+    hAxisRatio.GetYaxis().SetTitleOffset(1.9)
+    hAxisRatio.GetXaxis().SetTitleOffset(2.9)
+    hAxisRatio.GetYaxis().SetRangeUser(config["D0JetRate"]["minr"], config["D0JetRate"]["maxr"])
+    hAxisRatio.GetYaxis().SetNdivisions(509)
+    if "x_axis_title" in config:
+        hAxisRatio.GetXaxis().SetTitle(config["x_axis_title"])
+
+    # First plot the cross section + ratio panel
+
+    cname = "{}_{}".format(config["D0JetRate"]["name_prefix"], config["name"])
+    if "canvas_h" in config:
+        canvas_h = config["canvas_h"]
+    else:
+        canvas_h = 900
+    if "canvas_w" in config:
+        canvas_w = config["canvas_w"]
+    else:
+        canvas_w = 700
+    canvas = ROOT.TCanvas(cname, cname, canvas_w, canvas_h)
+    globalList.append(canvas)
+    canvas.Divide(1, 2)
+    padMain = canvas.cd(1)
+    padMain.SetPad(0, 0.35, 1, 1)
+    padMain.SetBottomMargin(0)
+    padMain.SetLeftMargin(0.15)
+    padMain.SetTicks(1, 1)
+    if config["logy"]: padMain.SetLogy()
+    padRatio = canvas.cd(2)
+    padRatio.SetPad(0, 0., 1, 0.35)
+    padRatio.SetTopMargin(0)
+    padRatio.SetBottomMargin(0.27)
+    padRatio.SetLeftMargin(0.15)
+    padRatio.SetGridy()
+    padRatio.SetTicks(1, 1)
+
+    padMain.cd()
+    hAxisCopy = hAxis.DrawCopy("axis")
+    globalList.append(hAxisCopy)
+
+    d0jet_syst_copy.Draw("2")
+    d0jet_stat_copy.Draw("same p x0 e0")
+    incl_syst_copy.Draw("2")
+    incl_stat_copy.Draw("same p x0 e0")
+
     for t in inclusive_jet_cross_sections.itervalues():
-        padMain.cd()
-        incl_h = t["histogram"].Clone("_".join([t["gen"], t["proc"]]))
-        incl_h.Draw("same e0") #e0 p x0")
-        globalList.append(incl_h)
-        incl_h.SetLineColor(t["color"])
-        incl_h.SetLineStyle(t["line"])
-        incl_h.SetLineWidth(2)
-        incl_h.SetMarkerColor(t["color"])
-        t["histogram_plot"] = incl_h
+        incl_h = t["histogram_plot"]
+        incl_h.Draw("same e0")
 
     for t in config["theory"]:
         if not t["active"]: continue
         if not t["inclusive"]: continue
-        padMain.cd()
-        d0jet_h = t["histogram"].Clone("_".join([t["gen"], t["proc"]]))
-        d0jet_h.Draw("same e0") #e0 p x0")
-        globalList.append(d0jet_h)
-        d0jet_h.SetLineColor(t["color"])
-        d0jet_h.SetLineStyle(t["line"])
-        d0jet_h.SetLineWidth(2)
-        d0jet_h.SetMarkerColor(t["color"])
-        t["histogram_plot"] = d0jet_h
+        d0jet_h = t["histogram_plot"]
+        d0jet_h.Draw("same e0")
 
-        incl_h = t["inclusive"]["histogram_plot"]
+    padRatio.cd()
+    hAxisRatioCopy = hAxisRatio.DrawCopy("axis")
+    globalList.append(hAxisRatioCopy)
 
-        padRatio.cd()
-        hratio = d0jet_h.Clone("_".join([t["gen"], t["proc"], "over", t["inclusive"]["gen"], t["inclusive"]["proc"]]))
-        hratio.Divide(incl_h)
-        hratio.Draw("same e0") # p x0")
-        t["ratio_histogram"] = hratio
+    ratioSyst.Draw("2")
+    ratioStat.Draw("same p x0 e0")
+
+    for t in config["theory"]:
+        if not t["active"]: continue
+        if not t["inclusive"]: continue
+        hratio = t["ratio_histogram"]
+        hratio.Draw("same")
+
+    # Now plotting labels
 
     padMain.cd()
 
     y1 = 0.87
     y2 = y1 - 0.06 * len(config["D0JetRate"]["title"])
-    padMain.cd()
     paveALICE = ROOT.TPaveText(0.16, y1, 0.55, y2, "NB NDC")
     globalList.append(paveALICE)
     paveALICE.SetBorderSize(0)
